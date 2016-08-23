d3bf68588f44519eb568a6d09b3662b5aa780a73
BenjaminBini <benjamin.bini@gmail.com>
2016-08-23 17:26:57 +0200

    Correction nom portlet front-end "Artwork"

--- modules/utils/src/main/java/eu/strasbourg/utils/constants/StrasbourgPortletKeys.java
+++ modules/utils/src/main/java/eu/strasbourg/utils/constants/StrasbourgPortletKeys.java
@@ -4,5 +4,5 @@ public class StrasbourgPortletKeys {
 	public static final String EDITION_BO = "eu_strasbourg_portlet_edition_EditionBOPortlet";
 	public static final String EDITION_WEB = "eu_strasbourg_portlet_edition_EditionPortlet";
 	public static final String ARTWORK_BO = "eu_strasbourg_portlet_artwork_ArtworkBOPortlet";
-	public static final String ARTWORK_WEB = "eu_strasbourg_portlet_artwork_ArtworkWebPortlet";
+	public static final String ARTWORK_WEB = "eu_strasbourg_portlet_artwork_ArtworkPortlet";
 }
