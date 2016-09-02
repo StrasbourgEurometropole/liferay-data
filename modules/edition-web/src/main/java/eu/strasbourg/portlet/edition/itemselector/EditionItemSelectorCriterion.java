b353b3c4ecbcc9a90a6fe41217efd2b0f8665142
BenjaminBini <benjamin.bini@gmail.com>
2016-09-02 16:13:45 +0200

    Ajout support item-selector pour editions et galeries

---
+++ modules/edition-web/src/main/java/eu/strasbourg/portlet/edition/itemselector/EditionItemSelectorCriterion.java
@@ -0,0 +1,7 @@
+package eu.strasbourg.portlet.edition.itemselector;
+
+import com.liferay.item.selector.BaseItemSelectorCriterion;
+
+public class EditionItemSelectorCriterion extends BaseItemSelectorCriterion {
+
+}
