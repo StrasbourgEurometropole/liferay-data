d20d4dc334665096f048122cd09ca83016aab4be
BenjaminBini <benjamin.bini@gmail.com>
2016-09-02 16:14:15 +0200

    Ajout support item-selector pour artwork et collections

---
+++ modules/artwork-web/src/main/java/eu/strasbourg/portlet/artwork/itemselector/ArtworkCollectionItemSelectorView.java
@@ -0,0 +1,123 @@
+package eu.strasbourg.portlet.artwork.itemselector;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.URLItemSelectorReturnType;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import eu.strasbourg.service.artwork.model.ArtworkCollection;
+import eu.strasbourg.service.artwork.service.ArtworkCollectionLocalServiceUtil;
+
+@Component(
+	property = { "item.selector.view.order:Integer=200" },
+	service = ItemSelectorView.class)
+public class ArtworkCollectionItemSelectorView
+	implements ItemSelectorView<ArtworkCollectionItemSelectorCriterion> {
+
+	private ServletContext _servletContext;
+
+	private static final List<ItemSelectorReturnType> _supportedItemSelectorReturnTypes = Collections
+		.unmodifiableList(ListUtil.fromArray(
+			new ItemSelectorReturnType[] { new URLItemSelectorReturnType() }));
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(target = "(osgi.web.symbolicname=artwork.web)", unbind = "-")
+	public void setServletContext(ServletContext servletContext) {
+		this._servletContext = servletContext;
+	}
+
+	@Override
+	public Class<ArtworkCollectionItemSelectorCriterion> getItemSelectorCriterionClass() {
+		return ArtworkCollectionItemSelectorCriterion.class;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return "Galeries";
+	}
+
+	@Override
+	public boolean isShowSearch() {
+		return true;
+	}
+
+	@Override
+	public boolean isVisible(ThemeDisplay themeDisplay) {
+		return true;
+	}
+
+	@Override
+	public void renderHTML(ServletRequest servletRequest,
+		ServletResponse servletResponse,
+		ArtworkCollectionItemSelectorCriterion itemSelectorCriterion,
+		PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay) servletRequest
+			.getAttribute(WebKeys.THEME_DISPLAY);
+
+		boolean multiple = GetterUtil
+			.getBoolean(servletRequest.getParameter("multiple"), false);
+
+		int delta = GetterUtil.getInteger(
+			servletRequest.getParameter(SearchContainer.DEFAULT_DELTA_PARAM),
+			SearchContainer.DEFAULT_DELTA);
+
+		int cur = GetterUtil.getInteger(
+			servletRequest.getParameter(SearchContainer.DEFAULT_CUR_PARAM),
+			SearchContainer.DEFAULT_CUR);
+
+		String keywords = GetterUtil
+			.getString(servletRequest.getParameter("keywords"));
+
+		List<ArtworkCollection> collections = ArtworkCollectionLocalServiceUtil
+			.findByKeyword(keywords, themeDisplay.getSiteGroupIdOrLiveGroupId(),
+				(delta * cur) - delta, ((delta * cur) + delta));
+
+		long collectionsCount = ArtworkCollectionLocalServiceUtil.findByKeywordCount(
+			keywords, themeDisplay.getSiteGroupIdOrLiveGroupId());
+
+		servletRequest.setAttribute("total", collectionsCount);
+		servletRequest.setAttribute("galleries", collections);
+		servletRequest.setAttribute("portletURL", portletURL);
+		servletRequest.setAttribute("itemSelectedEventName",
+			itemSelectedEventName);
+		servletRequest.setAttribute("multiple", multiple);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher = servletContext
+			.getRequestDispatcher(
+				"/artwork-collection/item-selector/collection-item-selector.jsp");
+
+		requestDispatcher.include(servletRequest, servletResponse);
+	}
+
+}
---
+++ modules/artwork-web/src/main/resources/META-INF/resources/artwork-collection/item-selector/collection-item-selector.jsp
@@ -0,0 +1,98 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %>
+<%@ taglib uri="http://liferay.com/tld/portlet" prefix="liferay-portlet" %>
+<%@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
+<%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+<%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+<%@ taglib uri="http://liferay.com/tld/frontend" prefix="liferay-frontend" %>
+
+<%@page import="com.liferay.portal.kernel.dao.search.RowChecker"%>
+
+<liferay-theme:defineObjects />
+
+<portlet:defineObjects />
+
+<div id="<portlet:namespace />productsImageSelectorWrapper" class="container-fluid-1280 main-content-body">
+
+	<liferay-ui:search-container
+		emptyResultsMessage="there-are-no-collections"
+		iteratorURL="${portletURL}"
+		total="${total}"
+		rowChecker="<%=new RowChecker(renderResponse) %>"
+	>
+		<liferay-ui:search-container-results
+			results="${collections}"
+		/>
+
+		<liferay-ui:search-container-row
+			className="eu.strasbourg.service.artwork.model.ArtworkCollection"
+			modelVar="collection" cssClass="collection-row" keyProperty="collectionId" rowIdProperty="collectionId"
+		>
+			<liferay-ui:search-container-column-text cssClass="content-column"
+				name="title" truncate="true"
+				value="${collection.titleCurrentValue}" />
+
+			<fmt:formatDate value="${collection.modifiedDate}"
+				var="formattedModifiedDate" type="date" pattern="dd/MM/yyyy HH:mm" />
+			<liferay-ui:search-container-column-text cssClass="content-column"
+				name="modified-date" truncate="true"
+				value="${formattedModifiedDate}" />
+
+			<liferay-ui:search-container-column-text cssClass="content-column"
+				truncate="true" name="status">
+				<liferay-ui:message
+					key="${collection.status ? 'published' : 'unpublished' }" />
+				<div class="data" data-id="${collection.collectionId}" data-title="${collection.getTitle(locale)}"></div>
+			</liferay-ui:search-container-column-text>
+			
+		</liferay-ui:search-container-row>
+
+		<liferay-ui:search-iterator  />
+
+		<liferay-ui:search-paginator searchContainer="${searchContainer}"   />
+	</liferay-ui:search-container>
+</div>
+
+<aui:script>
+	$('.collection-row input[type=checkbox]').on('change', function() {
+		var multiple = ${multiple};
+		if (!multiple) {
+			if (this.checked) {
+				$('input[type=checkbox]').prop('checked', false);
+				$('input[type=checkbox]').parents('tr').removeClass('info');
+				this.checked = true;
+				$(this).parents('tr').addClass('info');
+			}
+			Liferay.Util.getOpener().Liferay.fire('${itemSelectedEventName}', {
+				data: {
+					entityId: $(this).find('.data').data('id'),
+					title: $(this).find('.data').data('title')
+				}
+			});
+		} else {
+			var dataDivs = $('.info .data');
+			var dataToSend = [];
+			for (let i = 0; i < dataDivs.length; i++) {
+				dataToSend.push({
+					entityId: $(dataDivs[i]).data('id'),
+					title: $(dataDivs[i]).data('title')
+				});
+			}
+				
+			Liferay.Util.getOpener().Liferay.fire('${itemSelectedEventName}', {
+				data: dataToSend
+			});
+		}
+	});
+</aui:script>
+<style>
+	.table-first-header input {
+	    display: none;
+	}
+</style>
