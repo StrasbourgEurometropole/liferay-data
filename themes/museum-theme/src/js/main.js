017a632727c9b5595d367990938f02899d5ed4f1
BenjaminBini <benjamin.bini@gmail.com>
2016-09-20 11:35:35 +0200

    museum-theme : lint js

--- themes/museum-theme/src/js/main.js
+++ themes/museum-theme/src/js/main.js
@@ -1,19 +1,18 @@
 // Important : wrap it with AUI "all portlet ready" code
-
 // Dropdown langues
-(function ($) {
+(function($) {
   $('.language-menu a').on('click', function(e) {
     e.preventDefault();
     $('.language-menu').toggleClass('open');
   });
-}) (jQuery);
+})(jQuery);
 
 // Sticky (uniquement en desktop)
-(function ($) {
+(function($) {
   // Cas où il n'y a pas de menu musée
   if ($('.museum-header').length === 0) {
     $(document).on('scroll', function() {
-      if($(this).scrollTop() > 132) {
+      if ($(this).scrollTop() > 132) {
         $('.menu-header').addClass('sticky');
         $('body').css('margin-top', '49px');
       } else {
@@ -23,7 +22,7 @@
     });
   } else { // Avec menu musée
     $(document).on('scroll', function() {
-      if($(this).scrollTop() > 180) {
+      if ($(this).scrollTop() > 180) {
         $('.breadcrumb').addClass('sticky');
         $('.museum-header').addClass('sticky');
         $('body').css('margin-top', '86px');
@@ -34,21 +33,21 @@
       }
     });
   }
-}) (jQuery);
+})(jQuery);
 
 // Menu mobile
-(function ($) {
+(function($) {
   /*** 
    * Run this code when the #nav-toggle link has been tapped
    * or clicked
    */
-  $('.mobile-menu-toggle').on('touchstart click', function (e) {
+  $('.mobile-menu-toggle').on('touchstart click', function(e) {
     e.preventDefault();
     $('.mobile-menu-toggle').toggleClass("active");
     var $body = $('body'),
       $page = $('#page'),
 
-    /* Cross browser support for CSS "transition end" event */
+      /* Cross browser support for CSS "transition end" event */
       transitionEnd = 'transitionend webkitTransitionEnd otransitionend MSTransitionEnd';
 
     /* When the toggle menu link is clicked, animation starts */
@@ -64,7 +63,7 @@
       $page.off('touchstart click');
     } else {
       $body.addClass('left');
-      $page.on('touchstart click', function (e) {
+      $page.on('touchstart click', function(e) {
         if (!$body.hasClass('animating')) {
           e.preventDefault();
           $('#nav-toggle').trigger('click');
@@ -78,16 +77,16 @@
      * either add or remove the "menu-visible" class 
      * depending whether it was visible or not previously.
      */
-    $page.on(transitionEnd, function () {
+    $page.on(transitionEnd, function() {
       $body.removeClass('animating left right').toggleClass('menu-visible');
       $page.off(transitionEnd);
     });
   });
-}) (jQuery);
+})(jQuery);
 
 
 // Gestion des sous-menus mobiles
-(function ($) {
+(function($) {
   $('.mobile-main-menu a.submenu-toggle').on('click', function(e) {
     e.preventDefault();
     $('.mobile-main-menu .open').removeClass('open');
@@ -106,7 +105,7 @@
     e.preventDefault();
     var openedSubmenu = $('.mobile-main-menu .open');
     var nextOneToBeOpened = openedSubmenu.parent().parent();
-    if(nextOneToBeOpened.hasClass('main-menu') || nextOneToBeOpened.hasClass('submenu')) {
+    if (nextOneToBeOpened.hasClass('main-menu') || nextOneToBeOpened.hasClass('submenu')) {
       $('.mobile-main-menu .open').removeClass('open');
       nextOneToBeOpened.addClass('open');
     }
@@ -114,60 +113,63 @@
       $('.mobile-main-menu-nav').hide();
     }
   });
-}) (jQuery);
+})(jQuery);
 
 // Menu "accès par public" mobile
-(function ($) {
+(function($) {
   $('.accessibility-mobile-menu .has-submenu > a').on('click', function(e) {
     e.preventDefault();
     $('.accessibility-mobile-menu .access-by-public-submenu').toggleClass('open');
   });
-}) (jQuery);
+})(jQuery);
 
 // Carousel home
 (function($) {
   $(document).ready(function() {
-    var carousel = $('.home-carousel .owl-carousel').owlCarousel({
-      items: 1,
-      nav: true,
-      loop: true,
-      dotsContainer: '#carousel-custom-dots'
-    });
-    $('.home-carousel .owl-dot').click(function () {
-      carousel.trigger('to.owl.carousel', [$(this).index(), 300]);
-    });
+    if ($().owlCarousel) {
+      var carousel = $('.home-carousel .owl-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        dotsContainer: '#carousel-custom-dots'
+      });
+      $('.home-carousel .owl-dot').click(function() {
+        carousel.trigger('to.owl.carousel', [$(this).index(), 300]);
+      });
+      $('.home-carousel .owl-dot').hover(function() {
+        var dot = $(this);
+        $('.home-carousel .museum-name').text(dot.data('title'));
+      });
+      $('.home-carousel #carousel-custom-dots').mouseleave(function() {
+        var museumName = $('.home-carousel .museum-name');
+        museumName.text(museumName.data('original'));
+      });
+    }
   });
-  $('.home-carousel .owl-dot').hover(function() {
-    var dot = $(this);
-    $('.home-carousel .museum-name').text(dot.data('title'));
-  });
-  $('.home-carousel #carousel-custom-dots').mouseleave(function() {
-    var museumName = $('.home-carousel .museum-name');
-    museumName.text(museumName.data('original'));
-  });
-}) (jQuery);
+})(jQuery);
 
 // Items carousel
 (function($) {
   $(document).ready(function() {
-	// Carousel news
-    $('.items-carousel.news-carousel .owl-carousel').owlCarousel({
-      items: 1,
-      nav: true,
-      stagePadding: 50,
-      responsiveBaseElement: '.items-carousel.news-carousel',
-      responsive: {
-        601: {
-          stagePadding: 0
-        },
-        901: {
-          stagePadding: 0,
-          items: 2
+    if ($().owlCarousel) {
+      // Carousel news
+      $('.items-carousel.news-carousel .owl-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        stagePadding: 50,
+        responsiveBaseElement: '.items-carousel.news-carousel',
+        responsive: {
+          601: {
+            stagePadding: 0
+          },
+          901: {
+            stagePadding: 0,
+            items: 2
+          }
         }
-      }
-    });
-    // Carousel editions
-    $('.items-carousel.editions-carousel .owl-carousel').owlCarousel({
+      });
+      // Carousel editions
+      $('.items-carousel.editions-carousel .owl-carousel').owlCarousel({
         items: 1,
         nav: true,
         responsiveBaseElement: '.items-carousel.editions-carousel',
@@ -180,24 +182,27 @@
           }
         }
       });
+    }
+    if ($().dotdotdot) {
+	    $('.items-carousel .item-content').dotdotdot({
+	        watch: window
+	    });
+    }
   });
-  $('.items-carousel .item-content').dotdotdot({
-    watch: window
-  });
-}) (jQuery);
+})(jQuery);
 
 // Onglets expositions
 (function($) {
-	$(document).ready(function() {
-		$('.exhibition-tab').on('click', function(e) {
-			e.preventDefault();
-			
-			$('.exhibition-tab').removeClass('active');
-			$(this).addClass('active');
+  $(document).ready(function() {
+    $('.exhibition-tab').on('click', function(e) {
+      e.preventDefault();
 
-			var entryId = $(this).data('entry-id');
-			$('.exhibition-tab-content').removeClass('active');
-			$('.exhibition-tab-content[data-entry-id=' + entryId + ']').addClass('active');
-		});
-	});
-}) (jQuery);
+      $('.exhibition-tab').removeClass('active');
+      $(this).addClass('active');
+
+      var entryId = $(this).data('entry-id');
+      $('.exhibition-tab-content').removeClass('active');
+      $('.exhibition-tab-content[data-entry-id=' + entryId + ']').addClass('active');
+    });
+  });
+})(jQuery);
