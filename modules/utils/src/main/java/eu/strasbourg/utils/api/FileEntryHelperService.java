113c454d30fd8e5073bd76243b3955a31a5facd8
BenjaminBini <benjamin.bini@gmail.com>
2016-11-09 12:32:28 +0100

    utils : correction typo

--- modules/utils/src/main/java/eu/strasbourg/utils/api/FileEntryHelperService.java
+++ modules/utils/src/main/java/eu/strasbourg/utils/api/FileEntryHelperService.java
@@ -6,7 +6,7 @@ import aQute.bnd.annotation.ProviderType;
 
 /**
  * Classe de service qui peut être utilisée dans les templates
- * L'implèmentation appelle des fonctions statiques présentes dans les classes Helper classiques
+ * L'implémentation appelle des fonctions statiques présentes dans les classes Helper classiques
  * @author Benjamin Bini
  *
  */
