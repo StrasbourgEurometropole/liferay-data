eaea912b216b7dc5c8462df3ad6aeea4aa0c59d7
BenjaminBini <benjamin.bini@gmail.com>
2016-12-20 09:47:19 +0100

    all : désactivation fonctionnement SPA sur portlets BO

--- modules/edition-bo/src/main/java/eu/strasbourg/portlet/edition/EditionBOPortlet.java
+++ modules/edition-bo/src/main/java/eu/strasbourg/portlet/edition/EditionBOPortlet.java
@@ -26,6 +26,7 @@ import eu.strasbourg.portlet.edition.display.context.ViewGalleriesDisplayContext
 	immediate = true,
 	property = { "com.liferay.portlet.instanceable=false",
 		"com.liferay.portlet.footer-portlet-javascript=/js/edition-bo-main.js",
+		"com.liferay.portlet.single-page-application=false",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/edition-bo-view.jsp",
 		"javax.portlet.resource-bundle=content.Language",
