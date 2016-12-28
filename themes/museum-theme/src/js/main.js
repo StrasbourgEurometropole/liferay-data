899494996b7135545666830010dac052b230a85a
BenjaminBini <benjamin.bini@gmail.com>
2016-12-28 10:26:31 +0100

    museum-theme :  ajout d'un bouton de recherche

--- themes/museum-theme/src/js/main.js
+++ themes/museum-theme/src/js/main.js
@@ -329,10 +329,13 @@
 // Validation formulaire de recherche
 (function($) {
     $(document).ready(function() {
+        $("#keywords-deported-form input[type=text]").on("keyup paste", function() {
+            $(".search-asset-form input[type=hidden][name$=keywords]").val($(this).val());
+        });
         $('#keywords-deported-form').on('submit', function(e) {
             e.preventDefault();
-            var keywords = $('#keywords-deported-form input[type=text]').val();
-            $('.search-asset-form input[type=hidden][name$=keywords]').val(keywords);
+            //var keywords = $('#keywords-deported-form input[type=text]').val();
+            //$('.search-asset-form input[type=hidden][name$=keywords]').val(keywords);
             $('.search-asset-form form').submit();
         });
     });
