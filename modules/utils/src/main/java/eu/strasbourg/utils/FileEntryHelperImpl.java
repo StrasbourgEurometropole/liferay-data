f8ed1202329ff30f7e1d40dbffead81cfddd607b
BenjaminBini <benjamin.bini@gmail.com>
2017-03-22 09:39:11 +0100

    utils : ajout de fonction permettant de récupérer un fichier depuis son URL

--- modules/utils/src/main/java/eu/strasbourg/utils/FileEntryHelperImpl.java
+++ modules/utils/src/main/java/eu/strasbourg/utils/FileEntryHelperImpl.java
@@ -4,26 +4,24 @@ import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 
+import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import eu.strasbourg.utils.api.FileEntryHelperService;
 
 /**
- * Implémentation du service FileEntryHelperService
- * N'est qu'une couche accessible par les templates FreeMarker qui délègue le travail
- * à une classe helper
+ * Implémentation du service FileEntryHelperService N'est qu'une couche
+ * accessible par les templates FreeMarker qui délègue le travail à une classe
+ * helper
  */
 @Component(
-    immediate = true,
-    property = {
-    },
-    service = FileEntryHelperService.class
-)
+	immediate = true,
+	property = {},
+	service = FileEntryHelperService.class)
 public class FileEntryHelperImpl implements FileEntryHelperService {
 
 	@Override
-	public String getReadableFileEntrySize(long fileEntryId,
-		Locale locale) {
+	public String getReadableFileEntrySize(long fileEntryId, Locale locale) {
 		return FileEntryHelper.getReadableFileEntrySize(fileEntryId, locale);
 	}
 
@@ -36,15 +34,21 @@ public class FileEntryHelperImpl implements FileEntryHelperService {
 	public String getImageCopyright(long fileEntryId, Locale locale) {
 		return FileEntryHelper.getImageCopyright(fileEntryId, locale);
 	}
-	
+
 	@Override
 	public String getImageLegend(long fileEntryId, Locale locale) {
 		return FileEntryHelper.getImageLegend(fileEntryId, locale);
 	}
-	
+
 	@Override
-	public String getFileThumbnail(long fileEntryId, ThemeDisplay themeDisplay) {
+	public String getFileThumbnail(long fileEntryId,
+		ThemeDisplay themeDisplay) {
 		return FileEntryHelper.getFileThumbnail(fileEntryId, themeDisplay);
+	}
+
+	@Override
+	public DLFileEntry getFileEntryByRelativeURL(String url) {
+		return FileEntryHelper.getFileEntryByRelativeURL(url);
 	}
 
 }
--- modules/utils/src/main/java/eu/strasbourg/utils/api/FileEntryHelperService.java
+++ modules/utils/src/main/java/eu/strasbourg/utils/api/FileEntryHelperService.java
@@ -2,25 +2,29 @@ package eu.strasbourg.utils.api;
 
 import java.util.Locale;
 
+import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * Classe de service qui peut être utilisée dans les templates
- * L'implémentation appelle des fonctions statiques présentes dans les classes Helper classiques
+ * Classe de service qui peut être utilisée dans les templates L'implémentation
+ * appelle des fonctions statiques présentes dans les classes Helper classiques
+ * 
  * @author Benjamin Bini
  *
  */
 @ProviderType
 public interface FileEntryHelperService {
 	public String getReadableFileEntrySize(long fileEntryId, Locale locale);
-	
+
 	public String getFileEntryURL(long fileEntryId);
-	
+
 	public String getImageCopyright(long fileEntryId, Locale locale);
-	
+
 	public String getImageLegend(long fileEntryId, Locale locale);
-	
+
 	public String getFileThumbnail(long fileEntryId, ThemeDisplay themeDisplay);
+
+	public DLFileEntry getFileEntryByRelativeURL(String url);
 }
