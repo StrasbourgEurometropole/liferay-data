3a832d4b6a3c2ad182a4ef4645370001625d1d88
messagR <messagr@hotmail.com>
2017-04-19 15:07:34 +0200

    palce-bo : modification de l'input description

--- modules/place-bo/src/main/resources/META-INF/resources/js/place-bo-edit-subplace.js
+++ modules/place-bo/src/main/resources/META-INF/resources/js/place-bo-edit-subplace.js
@@ -133,7 +133,7 @@ jQuery(function() {
 					&& $(scheduleLabel).parents('.lfr-form-row').attr('class')
 							.indexOf('hide') == -1) {
 				var scheduleExceptionDescription = $(
-						namespaceAUI + "scheduleExceptionDescription" + index)
+						namespaceAUI + "scheduleExceptionDescription" + index + "_fr_FR")
 						.val();
 				var endDateSchedule = $(namespaceAUI + "endDateScheduleException" + index).val();
 				if(startDateSchedule == ""){
