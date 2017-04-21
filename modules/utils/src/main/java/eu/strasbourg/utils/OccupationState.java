1178921bf3505c7c85df272004feda1790141fe7
messagR <messagr@hotmail.com>
2017-04-21 12:28:52 +0200

    all : création du front tous les horaires

---
+++ modules/utils/src/main/java/eu/strasbourg/utils/OccupationState.java
@@ -0,0 +1,57 @@
+package eu.strasbourg.utils;
+
+public enum OccupationState {
+	
+    DISABLED("real-time-disabled", "white"),
+    NOT_AVAILABLE("not-available", "white"), 
+    GREEN("green-period", "green"), 
+    ORANGE("orange-period", "orange"),
+    RED("red-period", "red"),
+    BLACK("black-period", "black"),
+    FULL("full-period", "black"),
+    CLOSED("closed-period", "grey"),
+    OPEN("open-period", "green");
+    
+    String label;
+    String cssClass;
+    String occupation;
+    String capacity;
+    
+    OccupationState(String label, String cssClass) {
+	this.label = label;
+	this.cssClass = cssClass;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public String getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(String capacity) {
+		this.capacity = capacity;
+	}
+
+}
