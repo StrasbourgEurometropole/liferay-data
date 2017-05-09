b56dc21f721d1c1752a79d10cb4b1d433b73c5f0
BenjaminBini <benjamin.bini@gmail.com>
2017-05-09 10:55:23 +0200

    form-assembly-web : correction récupération paramètre sans namespace

--- modules/form-assembly-web/src/main/java/eu/strasbourg/portlet/formassembly/FormAssemblyPortlet.java
+++ modules/form-assembly-web/src/main/java/eu/strasbourg/portlet/formassembly/FormAssemblyPortlet.java
@@ -1,12 +1,17 @@
 package eu.strasbourg.portlet.formassembly;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -17,7 +22,6 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import eu.strasbourg.portlet.formassembly.configuration.FormAssemblyConfiguration;
@@ -27,6 +31,8 @@ import eu.strasbourg.portlet.formassembly.configuration.FormAssemblyConfiguratio
 	configurationPid = "eu.strasbourg.portlet.formassembly.configuration.FormAssemblyConfiguration",
 	property = { "com.liferay.portlet.display-category=Strasbourg",
 		"com.liferay.portlet.instanceable=false",
+		"com.liferay.portlet.requires-namespaced-parameters=false",
+		"com.liferay.portlet.single-page-application=false",
 		"com.liferay.portlet.css-class-wrapper=formassembly-portlet form-assembly-portlet",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/formassembly-view.jsp",
@@ -58,14 +64,13 @@ public class FormAssemblyPortlet extends MVCPortlet {
 			} else {
 				// On récupère l'html du formulaire (ou les résultats si les
 				// données viennent d'être saisies)
-				HttpServletRequest servletRequest = PortalUtil
-					.getHttpServletRequest(request);
+				Map<String, List<String>> params = splitQuery(themeDisplay.getURLCurrent());
+				List<String> tfaNexts = params != null ? params.get("tfa_next") : null;
+				String tfaNext = tfaNexts != null && tfaNexts.size() > 0 ? tfaNexts.get(0) : "";
 				FormAssemblyUtil formAssemblyUtil = new FormAssemblyUtil(
 					configuration.path(), configuration.token());
-				HttpServletRequest originalServletRequest = formAssemblyUtil
-					.getOriginalServletRequest(servletRequest);
 				String formHtml = formAssemblyUtil.getHTML(formId,
-					originalServletRequest.getParameter("tfa_next"));
+					tfaNext);
 				request.setAttribute("formHtml", formHtml);
 			}
 
@@ -75,6 +80,27 @@ public class FormAssemblyPortlet extends MVCPortlet {
 			_log.error(ex);
 		}
 	}
-	
+
+	private static Map<String, List<String>> splitQuery(String url)
+		throws UnsupportedEncodingException {
+		final Map<String, List<String>> query_pairs = new LinkedHashMap<String, List<String>>();
+		if (!url.contains("?")) {
+			return null;
+		}
+		final String[] pairs = url.split("\\?")[1].split("&");
+		for (String pair : pairs) {
+			final int idx = pair.indexOf("=");
+			final String key = idx > 0
+				? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
+			if (!query_pairs.containsKey(key)) {
+				query_pairs.put(key, new LinkedList<String>());
+			}
+			final String value = idx > 0 && pair.length() > idx + 1
+				? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
+			query_pairs.get(key).add(value);
+		}
+		return query_pairs;
+	}
+
 	private final Log _log = LogFactoryUtil.getLog(this.getClass().getName());
 }
