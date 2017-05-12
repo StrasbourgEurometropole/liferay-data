f96ed89940ca0c048daf86b0a94ce966217a7f43
BenjaminBini <benjamin.bini@gmail.com>
2017-05-12 09:49:13 +0200

    place-bo : récupération de l'accès handicapé et set à true de l'affichage des événements et de la soumission aux jours fériés

--- modules/place-bo/src/main/java/eu/strasbourg/portlet/place/action/ImportLegacyDataActionCommand.java
+++ modules/place-bo/src/main/java/eu/strasbourg/portlet/place/action/ImportLegacyDataActionCommand.java
@@ -47,37 +47,44 @@ public class ImportLegacyDataActionCommand implements MVCActionCommand {
 		int updatedPlacesCount = 0;
 		int placesWithoutLegacyCount = 0;
 		for (Place place : places) {
-			LegacyPlace legacyPlace = LegacyPlace
-				.fromSIGId(place.getSIGid(), Locale.FRANCE);
-			if (legacyPlace != null) {
+			LegacyPlace legacyPlace = LegacyPlace.fromSIGId(place.getSIGid(),
+				Locale.FRANCE);
+			if (legacyPlace != null && place.getSIGid().equals("408_SPO_9")) {
 				place.setAlias(legacyPlace.getAlias(), Locale.FRANCE);
-				place.setPresentation(legacyPlace.getPresentation(), Locale.FRANCE);
+				place.setPresentation(legacyPlace.getPresentation(),
+					Locale.FRANCE);
 				place.setServiceAndActivities(
 					legacyPlace.getServicesAndActivities(), Locale.FRANCE);
-				place.setCharacteristics(legacyPlace.getFeatures(), Locale.FRANCE);
+				place.setCharacteristics(legacyPlace.getFeatures(),
+					Locale.FRANCE);
 				place.setAccess(legacyPlace.getAccess(), Locale.FRANCE);
 				place.setAdditionalInformation(legacyPlace.getMoreInformation(),
 					Locale.FRANCE);
 				place.setSiteURL(legacyPlace.getWebsite(), Locale.FRANCE);
 				place.setSiteLabel(legacyPlace.getWebsiteName(), Locale.FRANCE);
-				place.setFacebookLabel(legacyPlace.getFacebookName(), Locale.FRANCE);
+				place.setFacebookLabel(legacyPlace.getFacebookName(),
+					Locale.FRANCE);
 				place.setAccessForBlind(legacyPlace.getAccessForBlind());
 				place.setAccessForDeaf(legacyPlace.getAccessForDeaf());
-				place.setAccessForDisabled(legacyPlace.getAccessForDisabled());
+				place.setAccessForWheelchair(
+					legacyPlace.getAccessForWheelchair());
 				place.setAccessForElder(legacyPlace.getAccessForElder());
 				place
 					.setAccessForDeficient(legacyPlace.getAccessForDeficient());
+				place.setAccessForDisabled(legacyPlace.getAccessForDisabled());
 				place.setPhone(legacyPlace.getPhone());
 				place.setMail(legacyPlace.getEmail());
 				updatedPlacesCount++;
 				System.out
 					.println("Mise à jour " + place.getAlias(Locale.FRANCE));
-				placeLocalService.updatePlace(place);
 			} else {
 				System.out
-				.println("Non-existant " + place.getAlias(Locale.FRANCE));
+					.println("Non-existant " + place.getAlias(Locale.FRANCE));
 				placesWithoutLegacyCount++;
 			}
+			place.setDisplayEvents(true);
+			place.setSubjectToPublicHoliday(true);
+			placeLocalService.updatePlace(place);
 		}
 		System.out.println("Lieux mis à jour : " + updatedPlacesCount
 			+ " - Lieux non existant dans LR6 " + placesWithoutLegacyCount);
