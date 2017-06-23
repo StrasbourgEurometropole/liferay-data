d40e01dd8d39fe4b6e0ea01bb382a3f4f654652d
BenjaminBini <benjamin.bini@gmail.com>
2017-06-23 15:31:06 +0200

    agenda-service : correction URL des images internes (utilisation de l'URL complète)

--- modules/agenda/agenda-service/src/main/java/eu/strasbourg/service/agenda/model/impl/EventImpl.java
+++ modules/agenda/agenda-service/src/main/java/eu/strasbourg/service/agenda/model/impl/EventImpl.java
@@ -101,7 +101,7 @@ public class EventImpl extends EventBaseImpl {
 		if (Validator.isNotNull(this.getExternalImageURL())) {
 			return this.getExternalImageURL();
 		} else {
-			return FileEntryHelper.getFileEntryURL(this.getImageId());
+			return StrasbourgPropsUtil.getURL() + FileEntryHelper.getFileEntryURL(this.getImageId());
 		}
 	}
 
