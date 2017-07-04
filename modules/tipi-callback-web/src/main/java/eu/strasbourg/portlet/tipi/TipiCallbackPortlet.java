6f4f51b9468dd71cff989a7968389f745b06a00d
BenjaminBini <benjamin.bini@gmail.com>
2017-07-04 17:20:13 +0200

    tipi-callback-web : mise en place callback

--- modules/tipi-callback-web/src/main/java/eu/strasbourg/portlet/tipi/TipiCallbackPortlet.java
+++ modules/tipi-callback-web/src/main/java/eu/strasbourg/portlet/tipi/TipiCallbackPortlet.java
@@ -16,6 +16,7 @@ import org.osgi.service.component.annotations.Component;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.upload.UploadServletRequestImpl;
 
 import eu.strasbourg.service.tipi.service.TipiEntryLocalService;
@@ -31,10 +32,11 @@ import eu.strasbourg.service.tipi.service.TipiEntryLocalService;
 		"javax.portlet.security-role-ref=power-user,user" },
 	service = Portlet.class)
 public class TipiCallbackPortlet extends MVCPortlet {
-	
+
 	private TipiEntryLocalService tipiEntryLocalService;
-	
-	public void setTipiEnryLocalService(TipiEntryLocalService tipiEntryLocalService) {
+
+	public void setTipiEnryLocalService(
+		TipiEntryLocalService tipiEntryLocalService) {
 		this.tipiEntryLocalService = tipiEntryLocalService;
 	}
 
@@ -45,17 +47,22 @@ public class TipiCallbackPortlet extends MVCPortlet {
 		// Utilisation de l'upload
 		UploadServletRequestImpl uploadServletRequest = new UploadServletRequestImpl(
 			PortalUtil.getHttpServletRequest(renderRequest));
-		
-		String objet = ParamUtil.getString(uploadServletRequest, "objet");
+
 		String price = ParamUtil.getString(uploadServletRequest, "montant");
-		String status = ParamUtil.getString(uploadServletRequest,"resultrans");
-		String dateString = ParamUtil.getString(uploadServletRequest, "dattrans");
-		
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("ddMMyyy");
-		LocalDate transactionDate = LocalDate.parse(dateString, fmt);
-		Date date = Date.from(transactionDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		
-		this.tipiEntryLocalService.addPayment(date, objet, status, price);
+		if (Validator.isNotNull(price)) {
+			String objet = ParamUtil.getString(uploadServletRequest, "objet");
+			String status = ParamUtil.getString(uploadServletRequest,
+				"resultrans");
+			String dateString = ParamUtil.getString(uploadServletRequest,
+				"dattrans");
+
+			DateTimeFormatter fmt = DateTimeFormatter.ofPattern("ddMMyyy");
+			LocalDate transactionDate = LocalDate.parse(dateString, fmt);
+			Date date = Date.from(transactionDate
+				.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+			this.tipiEntryLocalService.addPayment(date, objet, status, price);
+		}
 		super.render(renderRequest, renderResponse);
 	}
 
