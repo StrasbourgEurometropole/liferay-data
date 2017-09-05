b2d56e77447386812f21d34777bc2f4ec5f74a52
BenjaminBini <benjamin.bini@gmail.com>
2017-09-05 17:40:22 +0200

    strasbourg-theme : intégration modifications Telmedia

---
+++ themes/strasbourg-theme/custom/02-particules/02-modules/layouts/grids/grid-carrefour/grid-carrefour.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $('#seu-grid-carrefour').masonry({
+        itemSelector: '.seu-grid-item',
+        gutter: 20,
+        percentPosition: true,
+        transitionDuration: '0.6s'
+    });
+ }(jQuery));
