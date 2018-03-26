372b8dff4944ba5072a2319feff2dfaac85acc18
Benjamin Bini <benjamin@bini.io>
2018-03-26 12:21:33 +0200

    oidc-web : redirection vers l'URL du domaine en cours

--- modules/oidc-web/src/main/java/eu/strasbourg/filter/OIDCFilter.java
+++ modules/oidc-web/src/main/java/eu/strasbourg/filter/OIDCFilter.java
@@ -117,7 +117,7 @@ public class OIDCFilter extends BaseFilter {
 				// On récupère alors le JWT et l'access token via une requête
 				// vers le provider
 				LOG.info("Code received, requesting tokens");
-				JSONObject json = sendTokenRequest(request, response, filterChain, code);
+				JSONObject json = sendTokenRequest(request, code);
 				if (json == null) {
 					LOG.info("Token empty");
 					super.processFilter(request, response, filterChain);
@@ -207,8 +207,9 @@ public class OIDCFilter extends BaseFilter {
 	 */
 	private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.sendRedirect(
+
 		StrasbourgPropsUtil.getPublikAuthorizeURL()
-				+ "&redirect_uri=" + StrasbourgPropsUtil.getURL()
+				+ "&redirect_uri=" + this.getDomainRoot(request)
 				+ "&state=" + request.getRequestURL().toString());
 	}
 
@@ -216,8 +217,7 @@ public class OIDCFilter extends BaseFilter {
 	 * Envoi de la requête vers l'IdP afin de récupérer un access token et l'id
 	 * token
 	 */
-	private JSONObject sendTokenRequest(HttpServletRequest request, HttpServletResponse response,
-			FilterChain filterChain, String code) throws IOException {
+	private JSONObject sendTokenRequest(HttpServletRequest request, String code) throws IOException {
 		// Si oui, on récupère un token et les infos de l'utilisateur
 		String authURL = StrasbourgPropsUtil.getPublikTokenURL();
 
@@ -234,7 +234,7 @@ public class OIDCFilter extends BaseFilter {
 
 		// Paramètres
 		String parameters = "grant_type=authorization_code&code=" + code + "&redirect_uri="
-				+ StrasbourgPropsUtil.getURL();
+				+ this.getDomainRoot(request);
 		byte[] postData = parameters.getBytes(StandardCharsets.UTF_8);
 		int postDataLength = postData.length;
 		connection.setDoOutput(true);
@@ -252,8 +252,7 @@ public class OIDCFilter extends BaseFilter {
 		// Résultat
 		try {
 			InputStream is = connection.getInputStream();
-			JSONObject json = readJsonFromInputStream(is);
-			return json;
+			return readJsonFromInputStream(is);
 		} catch (Exception ex) {
 			BufferedReader rd = new BufferedReader(
 					new InputStreamReader(connection.getErrorStream(), Charset.forName("UTF-8")));
@@ -300,10 +299,9 @@ public class OIDCFilter extends BaseFilter {
 	private void redirectToIdPLogout(HttpServletRequest request, HttpServletResponse response) {
 		try {
 			response.sendRedirect(StrasbourgPropsUtil.getPublikLogoutURL()
-					+ "?post_logout_redirect_uri=" + StrasbourgPropsUtil.getURL()
+					+ "?post_logout_redirect_uri=" + this.getDomainRoot(request)
 					+ "&state=" + URLEncoder.encode(request.getRequestURL().toString(), "UTF-8"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -331,7 +329,6 @@ public class OIDCFilter extends BaseFilter {
 		String url = request.getRequestURL().toString();
 		String domain = getMainDomain(url);
 		cookie.setDomain(domain);
-		cookie.setMaxAge(60 * 60 * 10);
 		cookie.setPath("/");
 		response.addCookie(cookie);
 	}
@@ -374,20 +371,25 @@ public class OIDCFilter extends BaseFilter {
 	 * localhost), on renvoie le nom de domaine sans le sous domaine
 	 */
 	private String getMainDomain(String urlString) {
-		URL url = null;
 		String domainString = null;
 		try {
-			url = new URL(urlString);
+			URL url = new URL(urlString);
 			String[] domainNameParts = url.getHost().split("\\.");
 			domainString = domainNameParts[domainNameParts.length - 1];
 			if (domainNameParts.length > 1) {
 				domainString = "." + domainNameParts[domainNameParts.length - 2] + "." + domainString;
 			}
 
-		} catch (MalformedURLException e) {
+		} catch (MalformedURLException ignored) {
 		}
 
 		return domainString;
+	}
+
+	private String getDomainRoot(HttpServletRequest request) {
+		return request.getRequestURL().toString()
+				.replace(request.getServletPath(), "")
+				.replace(request.getPathInfo(), "");
 	}
 
 }
