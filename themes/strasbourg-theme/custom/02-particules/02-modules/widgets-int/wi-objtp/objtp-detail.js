77e8ebab5929c69f74e4a7881f4e527e6df77d99
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2018-04-25 11:06:44 +0200

    strasbourg-theme : modification pour objtp

---
+++ themes/strasbourg-theme/custom/02-particules/02-modules/widgets-int/wi-objtp/objtp-detail.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+    $('#objtp-detail-container').masonry({
+        itemSelector: '.objtp-detail-item',
+        gutter: 20,
+        percentPosition: true,
+        transitionDuration: '0.6s'
+    });
+});
