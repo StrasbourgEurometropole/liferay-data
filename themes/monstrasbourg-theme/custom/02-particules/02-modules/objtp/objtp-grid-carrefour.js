d3181b8fc719fd4287149c6d33fafb6257295352
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2018-04-25 11:06:08 +0200

    theme-monstrasbourg : enlevé les modifs liées à objtp

--- themes/monstrasbourg-theme/custom/02-particules/02-modules/objtp/objtp-grid-carrefour.js
+++
@@ -1,8 +0,0 @@
-$(window).load(function() {
-    $('#objtp-grid-carrefour').masonry({
-        itemSelector: '.objtp-grid-item',
-        gutter: 20,
-        percentPosition: true,
-        transitionDuration: '0.6s'
-    });
-});
