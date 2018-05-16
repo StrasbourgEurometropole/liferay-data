08cc6cc9c162bf18d6ac55f8675cba96bb433ff4
messagR <messagr@hotmail.com>
2018-05-16 09:20:28 +0200

    vote-place-web : création, suite et fin

--- modules/vote-place-web/src/main/java/eu/strasbourg/portlet/vote_place/VotePlaceWebPortlet.java
+++ modules/vote-place-web/src/main/java/eu/strasbourg/portlet/vote_place/VotePlaceWebPortlet.java
@@ -12,8 +12,6 @@ import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
@@ -21,11 +19,12 @@ import org.osgi.service.component.annotations.Component;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupModel;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.SessionParamUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -33,7 +32,9 @@ import com.liferay.portal.kernel.util.WebKeys;
 
 import eu.strasbourg.portlet.felec.portlet.FelecResponse;
 import eu.strasbourg.portlet.felec.portlet.FelecWebServiceClient;
-import eu.strasbourg.utils.PortletHelper;
+import eu.strasbourg.service.place.exception.NoSuchPlaceException;
+import eu.strasbourg.service.place.model.Place;
+import eu.strasbourg.service.place.service.PlaceLocalServiceUtil;
 import eu.strasbourg.utils.PublikApiClient;
 import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
 
@@ -41,18 +42,15 @@ import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
  * @author angelique.champougny
  */
 @Component(immediate = true, property = { "com.liferay.portlet.display-category=Strasbourg",
-		"com.liferay.portlet.instanceable=false", "com.liferay.portlet.required-namespaced-parameters=false",
+		"com.liferay.portlet.instanceable=true", "com.liferay.portlet.required-namespaced-parameters=false",
 		"javax.portlet.display-name=vote-place-web Portlet", "javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/vote-place-view.jsp",
 		"javax.portlet.name=" + StrasbourgPortletKeys.VOTE_PLACE_WEB, "javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=power-user,user" }, service = Portlet.class)
 public class VotePlaceWebPortlet extends MVCPortlet {
-	private String address;
 
 	@Override
 	public void render(RenderRequest request, RenderResponse response) throws IOException, PortletException {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
 
 		// récupération des infos de l'utilisateur
 		String name = null;
@@ -69,32 +67,47 @@ public class VotePlaceWebPortlet extends MVCPortlet {
 			birthDate = userDetail.getString("birthdate");
 			birthPlace = userDetail.getString("birthplace");
 		}
-		request.setAttribute("name", name);
+		request.setAttribute("lastName", name);
 		request.setAttribute("firstName", firstName);
 		request.setAttribute("birthDate", birthDate);
 		request.setAttribute("birthPlace", birthPlace);
 
 		FelecResponse felecResponse = null;
-		if (Validator.isNotNull(birthPlace) && Validator.isNotNull(birthDate)) {
+		if (Validator.isNotNull(birthPlace) && Validator.isNotNull(birthDate) && Validator.isNotNull(name)
+				&& Validator.isNotNull(firstName)) {
 			try {
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 				Date date = sdf.parse(birthDate);
 				felecResponse = FelecWebServiceClient.getResponse(name, firstName, date, birthPlace);
 				request.setAttribute("felecResponse", felecResponse);
+				Place office = null;
+				try {
+					office = PlaceLocalServiceUtil.findByName(
+							"Bureau de vote " + (felecResponse.getStationNumber().substring(0, 1).equals("0")
+									? felecResponse.getStationNumber().substring(1)
+									: felecResponse.getStationNumber()));
+				} catch (NoSuchPlaceException e) {
+					e.printStackTrace();
+				}
+				request.setAttribute("office", office);
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
-		}else{
+		} else {
 			List<String> champsNull = new ArrayList<String>();
-			if (Validator.isNotNull(birthPlace)){
+			if (Validator.isNull(birthPlace)) {
 				champsNull.add("Ville de naissance");
 			}
-			if (Validator.isNotNull(birthDate)){
+			if (Validator.isNull(birthDate)) {
 				champsNull.add("Date de naissance");
 			}
 			request.setAttribute("champsNull", champsNull);
 		}
 
+		Group group = GroupLocalServiceUtil.fetchFriendlyURLGroup(
+				((ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY)).getCompanyId(), "/strasbourg.eu");
+		String virtualHostName = group.getPublicLayoutSet().getVirtualHostname();
+		request.setAttribute("virtualHostName", virtualHostName);
 		super.render(request, response);
 	}
 
--- modules/vote-place-web/src/main/resources/META-INF/resources/vote-place-init.jsp
+++ modules/vote-place-web/src/main/resources/META-INF/resources/vote-place-init.jsp
@@ -2,6 +2,8 @@
 
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
 
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+
 <%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
 taglib uri="http://liferay.com/tld/portlet" prefix="liferay-portlet" %><%@
 taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %><%@
