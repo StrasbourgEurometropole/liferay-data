9d39b2319639df13881d54c50800cfa16e44b162
romain.vergnais <romain.vergnais@sully-group.fr>
2018-05-17 11:59:43 +0200

    projet-service : Correction statut du projet

--- modules/project/project-service/src/main/java/eu/strasbourg/service/project/model/impl/ProjectImpl.java
+++ modules/project/project-service/src/main/java/eu/strasbourg/service/project/model/impl/ProjectImpl.java
@@ -103,16 +103,30 @@ public class ProjectImpl extends ProjectBaseImpl {
 		List<ProjectTimeline> projectTimelines = ProjectTimelineLocalServiceUtil.getByProjectId(this.getProjectId());
 		return projectTimelines;
 	}
+
+	/*
+	 * Retourne la catégorie Territoire correspondant à la ville du lieu
+	 */
+	@Override
+	public String getProjectStatus(Locale locale) {
+		List<AssetCategory> pStatus = getAllStatus();
+		
+		String result = "";
+		
+		//Retourne le premier element de la liste
+		if(pStatus.size() >= 1)
+			result = pStatus.get(0).getTitle(locale);
+		
+		return result;
+	}
 	
 	/**
-	 * Retourne les territoire du lieu
+	 * Retourne les statuts du projet
 	 */
-//	@Override
-//	public List<AssetCategory> getTerritories() {
-//		
-//		AssetVocabularyHelper.getass
-//		return AssetVocabularyHelper.getAssetEntryCategoriesByVocabulary(this.getAssetEntry(),
-//				VocabularyNames.PROJECT_STATUS);
-//	}
+	@Override
+	public List<AssetCategory> getAllStatus() {
+		return AssetVocabularyHelper.getAssetEntryCategoriesByVocabulary(this.getAssetEntry(),
+				VocabularyNames.PROJECT_STATUS);
+	}
 	
 }
