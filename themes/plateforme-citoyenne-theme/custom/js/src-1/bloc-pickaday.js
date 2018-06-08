e21044b5842262740793f685bdba20678c207288
Cédric Henry <h.cedric.stylix@gmail.com>
2018-06-08 12:08:42 +0200

    search-asset-wab/placit : Pikaday --> résolution du format

--- themes/plateforme-citoyenne-theme/custom/js/src-1/bloc-pickaday.js
+++ themes/plateforme-citoyenne-theme/custom/js/src-1/bloc-pickaday.js
@@ -1,5 +1,20 @@
 // Afficher une date
 
 $('.frm_date').each(function(){
-    var picker = new Pikaday({ field: this,format: 'DD/MM/YY', });
+    var picker = new Pikaday({ 
+    	field: this,
+    	toString(date, format) {
+	        const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate() ;
+	        const month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+	        const year = date.getFullYear();
+	        return `${day}/${month}/${year}`;
+	    },
+	    parse(dateString, format) {
+	        const parts = dateString.split('/');
+	        const day = parseInt(parts[0], 10);
+	        const month = parseInt(parts[1] - 1, 10);
+	        const year = parseInt(parts[1], 10);
+	        return new Date(year, month, day);
+	    }
+    });
 });
--- themes/plateforme-citoyenne-theme/src/js/strasbourg.js
+++ themes/plateforme-citoyenne-theme/src/js/strasbourg.js
@@ -11208,7 +11208,25 @@ $('.bloc-iframe iframe').height(
 // Afficher une date
 
 $('.frm_date').each(function(){
-    var picker = new Pikaday({ field: this,format: 'DD/MM/YY', });
+    var picker = new Pikaday({ 
+    	field: this,
+    	toString(date, format) {
+        // you should do formatting based on the passed format,
+        // but we will just return 'D/M/YYYY' for simplicity
+        const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate() ;
+        const month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+	    },
+	    parse(dateString, format) {
+	        // dateString is the result of `toString` method
+	        const parts = dateString.split('/');
+	        const day = parseInt(parts[0], 10);
+	        const month = parseInt(parts[1] - 1, 10);
+	        const year = parseInt(parts[1], 10);
+	        return new Date(year, month, day);
+	    }
+    });
 });
 $('.pro-bloc-prefooter .pro-ico').on('click',function(e){
     e.preventDefault();
