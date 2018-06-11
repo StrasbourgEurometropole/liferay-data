cabea91079188c7a263007e39ee2e4eda3fd0992
romain.vergnais <romain.vergnais@sully-group.fr>
2018-06-11 12:09:07 +0200

    general : Breadcrumbs

--- modules/project/project-api/src/main/java/eu/strasbourg/service/project/service/persistence/ProjectPersistence.java
+++ modules/project/project-api/src/main/java/eu/strasbourg/service/project/service/persistence/ProjectPersistence.java
@@ -484,60 +484,6 @@ public interface ProjectPersistence extends BasePersistence<Project> {
 		throws NoSuchProjectException;
 
 	/**
-	* Returns all the projects that the user has permission to view where groupId = &#63;.
-	*
-	* @param groupId the group ID
-	* @return the matching projects that the user has permission to view
-	*/
-	public java.util.List<Project> filterFindByGroupId(long groupId);
-
-	/**
-	* Returns a range of all the projects that the user has permission to view where groupId = &#63;.
-	*
-	* <p>
-	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link ProjectModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
-	* </p>
-	*
-	* @param groupId the group ID
-	* @param start the lower bound of the range of projects
-	* @param end the upper bound of the range of projects (not inclusive)
-	* @return the range of matching projects that the user has permission to view
-	*/
-	public java.util.List<Project> filterFindByGroupId(long groupId, int start,
-		int end);
-
-	/**
-	* Returns an ordered range of all the projects that the user has permissions to view where groupId = &#63;.
-	*
-	* <p>
-	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link ProjectModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
-	* </p>
-	*
-	* @param groupId the group ID
-	* @param start the lower bound of the range of projects
-	* @param end the upper bound of the range of projects (not inclusive)
-	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
-	* @return the ordered range of matching projects that the user has permission to view
-	*/
-	public java.util.List<Project> filterFindByGroupId(long groupId, int start,
-		int end,
-		com.liferay.portal.kernel.util.OrderByComparator<Project> orderByComparator);
-
-	/**
-	* Returns the projects before and after the current project in the ordered set of projects that the user has permission to view where groupId = &#63;.
-	*
-	* @param projectId the primary key of the current project
-	* @param groupId the group ID
-	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
-	* @return the previous, current, and next project
-	* @throws NoSuchProjectException if a project with the primary key could not be found
-	*/
-	public Project[] filterFindByGroupId_PrevAndNext(long projectId,
-		long groupId,
-		com.liferay.portal.kernel.util.OrderByComparator<Project> orderByComparator)
-		throws NoSuchProjectException;
-
-	/**
 	* Removes all the projects where groupId = &#63; from the database.
 	*
 	* @param groupId the group ID
@@ -551,14 +497,6 @@ public interface ProjectPersistence extends BasePersistence<Project> {
 	* @return the number of matching projects
 	*/
 	public int countByGroupId(long groupId);
-
-	/**
-	* Returns the number of projects that the user has permission to view where groupId = &#63;.
-	*
-	* @param groupId the group ID
-	* @return the number of matching projects that the user has permission to view
-	*/
-	public int filterCountByGroupId(long groupId);
 
 	/**
 	* Caches the project in the entity cache if it is enabled.
