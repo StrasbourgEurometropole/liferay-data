0a368fed1da454f1d41eb6b574c365d009f17ca0
Cédric Henry <h.cedric.stylix@gmail.com>
2018-06-13 10:11:02 +0200

    theme-placit : Try-hard Pikaday

--- themes/plateforme-citoyenne-theme/src/js/strasbourg.js
+++ themes/plateforme-citoyenne-theme/src/js/strasbourg.js
@@ -9334,7 +9334,14 @@ var objectFitImages = (function () {
         toString: null,
 
         // used to create date object from current input string
-        parse: null,
+        parse: function (dateString, format) {
+            // dateString is the result of `toString` method
+            const parts = dateString.split('/');
+            const day = parseInt(parts[0], 10);
+            const month = parseInt(parts[1] - 1, 10);
+            const year = parseInt(parts[1], 10);
+            return new Date(year, month, day);
+        },
 
         // the initial date to view when first opened
         defaultDate: null,
@@ -11210,12 +11217,21 @@ $('.bloc-iframe iframe').height(
 $('.frm_date').each(function(){
     var picker = new Pikaday({ 
     	field: this,
+    	format: 'D/M/YYYY',
     	toString(date, format) {
 	        const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate() ;
 	        const month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
 	        const year = date.getFullYear();
 	        return `${day}/${month}/${year}`;
 	    },
+	    parse: function (dateString, format) {
+	        // dateString is the result of `toString` method
+	        const parts = dateString.split('/');
+	        const day = parseInt(parts[0], 10);
+	        const month = parseInt(parts[1] - 1, 10);
+	        const year = parseInt(parts[1], 10);
+	        return new Date(year, month, day);
+    	},	
 	    onSelect: function(date) {
             var name = picker._o.field.name;
             if ($('input[data-name="' + name + 'Day"').length) {
