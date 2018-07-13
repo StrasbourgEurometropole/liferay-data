d4274f7960f8339e905b720588eefc4cd950f5c1
messagR <messagr@hotmail.com>
2018-07-13 16:31:05 +0200

    utils et felec-web : ajout du timeout en clé de properties

--- modules/utils/src/main/java/eu/strasbourg/utils/StrasbourgPropsUtil.java
+++ modules/utils/src/main/java/eu/strasbourg/utils/StrasbourgPropsUtil.java
@@ -196,4 +196,8 @@ public class StrasbourgPropsUtil {
 	public static String getResidantURL(){
 		return PropsUtil.get("eu.strasbourg.resid.url");
 	}
+	
+	public static int getWebServiceDefaultTimeout() {
+		return Integer.parseInt(PropsUtil.get("eu.strasbourg.webservice.default.timeout"));
+	}
 }
