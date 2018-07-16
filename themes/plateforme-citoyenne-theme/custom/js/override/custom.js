7d0829a0a960f814d124a476599912176ac117e1
Cédric Henry <h.cedric.stylix@gmail.com>
2018-07-16 15:30:54 +0200

    themes-placit : adaptation pour effectuer les likes/dislikes sur les vignettes

--- themes/plateforme-citoyenne-theme/custom/js/override/custom.js
+++ themes/plateforme-citoyenne-theme/custom/js/override/custom.js
@@ -2,18 +2,25 @@
 * Gestion des likes/dislikes
 */
 $(function() {
-    $(document).on("click", '.pro-like, .pro-dislike', function(e) {
+    $(document).on("click", "[href$='-approuv'], [href$='like-pro']", function(e) {
 
         e.preventDefault();
         e.stopPropagation();
 
+        // Sauvegarde de l'élément
         var element = $(this);
+
+        // L'élément a t-il un affiche du compteur en texte ou en span ?
+        var counterType = $(this).attr('href').endsWith('-approuv') ? "span" : "text" ;
+
+        // Récupération des attributs du like
         var title = $(this).data("title");
         var isdislike = $(this).data("isdislike");
         var typeid = $(this).data("typeid");
         var entityid = $(this).data("entityid");
         var entitygroupid = $(this).data("entitygroupid") ? $(this).data("entitygroupid") : 0;
 
+        // Appel au service remote Liferay
         Liferay.Service(
             '/like.like/add-like-link',
             {
@@ -24,34 +31,61 @@ $(function() {
                 entityGroupId: entitygroupid
             },
             function(obj) {
-                // En cas de succès, on modifie le bouton
+                // En cas de succès, on effectue la modification des éléments visuels
+                // selon la réponse et le type de l'élément
                 if (obj.hasOwnProperty('success')) {
                     switch(obj['success']) {
                         case "like added":
                             element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) + 1);
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) + 1);
+                            } else {
+                                element.text(+parseInt(element.text()) + 1);
+                            }
                             break;
                         case "dislike added":
                             element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) + 1);
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) + 1);
+                            } else {
+                                element.text(+parseInt(element.text()) + 1);
+                            }
                             break;
                         case "like mind changed added":
                             element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) + 1);
-                            element.siblings().first().find('strong').text(+parseInt(element.siblings().first().text()) - 1);
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) + 1);
+                                element.siblings().first().find('strong').text(+parseInt(element.siblings().first().text()) - 1);
+                            } else {
+                                element.text(+parseInt(element.text()) + 1);
+                                element.siblings().first().text(+parseInt(element.siblings().first().text()) - 1);
+                            }
                             break;
                         case "dislike mind changed added":
                             element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) + 1);
-                            element.siblings().first().find('strong').text(+parseInt(element.siblings().first().text()) - 1);
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) + 1);
+                                element.siblings().first().find('strong').text(+parseInt(element.siblings().first().text()) - 1);
+                            } else {
+                                element.text(+parseInt(element.text()) + 1);
+                                element.siblings().first().text(+parseInt(element.siblings().first().text()) - 1);
+                            }
                             break;
                         case "like deleted":
                             element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) - 1);
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) - 1);
+                            } else {
+                                element.text(+parseInt(element.text()) - 1);
+                            }
                             break;
                         case "dislike deleted":
                             element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) - 1);
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) - 1);
+                            } else {
+                                element.text(+parseInt(element.text()) - 1);
+                            }
                             break;
                     }
 
--- themes/plateforme-citoyenne-theme/src/js/strasbourg.js
+++ themes/plateforme-citoyenne-theme/src/js/strasbourg.js
@@ -11324,81 +11324,6 @@ $('.pro-bloc-video').each(function() {
 //    $(this).find('strong').text(+parseInt($(this).text()) + 1);
 //    $(this).toggleClass('active');
 //});
-/*
-* Gestion des likes/dislikes
-*/
-$(function() {
-    $(document).on("click", '.pro-like, .pro-dislike', function(e) {
-
-        e.preventDefault();
-        e.stopPropagation();
-
-        var element = $(this);
-        var title = $(this).data("title");
-        var isdislike = $(this).data("isdislike");
-        var typeid = $(this).data("typeid");
-        var entityid = $(this).data("entityid");
-        var entitygroupid = $(this).data("entitygroupid") ? $(this).data("entitygroupid") : 0;
-
-        Liferay.Service(
-            '/like.like/add-like-link',
-            {
-                title: title,
-                isDislike: isdislike,
-                typeId: typeid,
-                entityId: entityid,
-                entityGroupId: entitygroupid
-            },
-            function(obj) {
-                // En cas de succès, on modifie le bouton
-                if (obj.hasOwnProperty('success')) {
-                    switch(obj['success']) {
-                        case "like added":
-                            element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) + 1);
-                            break;
-                        case "dislike added":
-                            element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) + 1);
-                            break;
-                        case "like mind changed added":
-                            element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) + 1);
-                            element.siblings().first().find('strong').text(+parseInt(element.siblings().first().text()) - 1);
-                            break;
-                        case "dislike mind changed added":
-                            element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) + 1);
-                            element.siblings().first().find('strong').text(+parseInt(element.siblings().first().text()) - 1);
-                            break;
-                        case "like deleted":
-                            element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) - 1);
-                            break;
-                        case "dislike deleted":
-                            element.toggleClass('active');
-                            element.find('strong').text(+parseInt(element.text()) - 1);
-                            break;
-                    }
-
-                }
-                // Sinon on affiche un message d'erreur
-                else if (obj.hasOwnProperty('error')) {
-                    if (obj['error'] == 'notConnected') {
-                        // Si l'utilisateur n'est pas connecté
-                        e.preventDefault();
-                        $("#myModal").modal();
-                    } else {
-                        // Autre erreur
-                        alert('Une erreur est survenue.');
-                    }
-                }
-            }
-        );
-
-    });
-});
-
 var bounds;
 var map;
 
@@ -12190,3 +12115,111 @@ var isiPad = navigator.userAgent.match(/iPad/i) != null;
 if (isiPad) {
     $('body').addClass('on-ipad');
 }
+/*
+* Gestion des likes/dislikes
+*/
+$(function() {
+    $(document).on("click", "[href$='-approuv'], [href$='like-pro']", function(e) {
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        // Sauvegarde de l'élément
+        var element = $(this);
+
+        // L'élément a t-il un affiche du compteur en texte ou en span ?
+        var counterType = $(this).attr('href').endsWith('-approuv') ? "span" : "text" ;
+
+        // Récupération des attributs du like
+        var title = $(this).data("title");
+        var isdislike = $(this).data("isdislike");
+        var typeid = $(this).data("typeid");
+        var entityid = $(this).data("entityid");
+        var entitygroupid = $(this).data("entitygroupid") ? $(this).data("entitygroupid") : 0;
+
+        // Appel au service remote Liferay
+        Liferay.Service(
+            '/like.like/add-like-link',
+            {
+                title: title,
+                isDislike: isdislike,
+                typeId: typeid,
+                entityId: entityid,
+                entityGroupId: entitygroupid
+            },
+            function(obj) {
+                // En cas de succès, on effectue la modification des éléments visuels
+                // selon la réponse et le type de l'élément
+                if (obj.hasOwnProperty('success')) {
+                    switch(obj['success']) {
+                        case "like added":
+                            element.toggleClass('active');
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) + 1);
+                            } else {
+                                element.text(+parseInt(element.text()) + 1);
+                            }
+                            break;
+                        case "dislike added":
+                            element.toggleClass('active');
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) + 1);
+                            } else {
+                                element.text(+parseInt(element.text()) + 1);
+                            }
+                            break;
+                        case "like mind changed added":
+                            element.toggleClass('active');
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) + 1);
+                                element.siblings().first().find('strong').text(+parseInt(element.siblings().first().text()) - 1);
+                            } else {
+                                element.text(+parseInt(element.text()) + 1);
+                                element.siblings().first().text(+parseInt(element.siblings().first().text()) - 1);
+                            }
+                            break;
+                        case "dislike mind changed added":
+                            element.toggleClass('active');
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) + 1);
+                                element.siblings().first().find('strong').text(+parseInt(element.siblings().first().text()) - 1);
+                            } else {
+                                element.text(+parseInt(element.text()) + 1);
+                                element.siblings().first().text(+parseInt(element.siblings().first().text()) - 1);
+                            }
+                            break;
+                        case "like deleted":
+                            element.toggleClass('active');
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) - 1);
+                            } else {
+                                element.text(+parseInt(element.text()) - 1);
+                            }
+                            break;
+                        case "dislike deleted":
+                            element.toggleClass('active');
+                            if (counterType === "span") {
+                                element.find('strong').text(+parseInt(element.text()) - 1);
+                            } else {
+                                element.text(+parseInt(element.text()) - 1);
+                            }
+                            break;
+                    }
+
+                }
+                // Sinon on affiche un message d'erreur
+                else if (obj.hasOwnProperty('error')) {
+                    if (obj['error'] == 'notConnected') {
+                        // Si l'utilisateur n'est pas connecté
+                        e.preventDefault();
+                        $("#myModal").modal();
+                    } else {
+                        // Autre erreur
+                        alert('Une erreur est survenue.');
+                    }
+                }
+            }
+        );
+
+    });
+});
