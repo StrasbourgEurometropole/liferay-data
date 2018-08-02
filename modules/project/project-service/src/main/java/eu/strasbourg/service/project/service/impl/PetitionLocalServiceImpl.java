72bef85c83522402f73d62d247881f7351b30d69
alexandre.quere <alexandre.quere@sully-group.fr>
2018-08-02 16:42:39 +0200

    petition rendu assetRenderer

---
+++ modules/project/project-service/src/main/java/eu/strasbourg/service/project/service/impl/PetitionLocalServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package eu.strasbourg.service.project.service.impl;
+
+import eu.strasbourg.service.project.service.base.PetitionLocalServiceBaseImpl;
+
+/**
+ * The implementation of the petition local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link eu.strasbourg.service.project.service.PetitionLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author Cedric Henry
+ * @see PetitionLocalServiceBaseImpl
+ * @see eu.strasbourg.service.project.service.PetitionLocalServiceUtil
+ */
+public class PetitionLocalServiceImpl extends PetitionLocalServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use {@link eu.strasbourg.service.project.service.PetitionLocalServiceUtil} to access the petition local service.
+	 */
+}
