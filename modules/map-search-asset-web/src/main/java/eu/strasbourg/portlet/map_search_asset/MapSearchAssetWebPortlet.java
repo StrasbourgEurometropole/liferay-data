5910e888e67e7f5fabc7410b0952f996736188a5
Cédric Henry <h.cedric.stylix@gmail.com>
2018-08-08 17:52:52 +0200

    map-search-asset-web : création de l'event du formulaire et préparation des variables temp des résultats

--- modules/map-search-asset-web/src/main/java/eu/strasbourg/portlet/map_search_asset/MapSearchAssetWebPortlet.java
+++ modules/map-search-asset-web/src/main/java/eu/strasbourg/portlet/map_search_asset/MapSearchAssetWebPortlet.java
@@ -1,19 +1,29 @@
 package eu.strasbourg.portlet.map_search_asset;
 
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.SessionParamUtil;
 
+import eu.strasbourg.service.agenda.model.Event;
+import eu.strasbourg.service.project.model.Participation;
+import eu.strasbourg.service.project.model.Project;
+import eu.strasbourg.utils.AssetVocabularyHelper;
 import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
@@ -37,9 +47,44 @@ import org.osgi.service.component.annotations.Component;
 )
 public class MapSearchAssetWebPortlet extends MVCPortlet {
 	
+	private static final String CITY_NAME = "Strasbourg";
+	
+	private long _selectedDistrictCategoryId;
+	private List<Project> _projects;
+	private List<Participation> _participations;
+	private List<Event> _events;
+	
+	/**
+	 * Initialisation de la vue
+	 */
 	@Override
 	public void render(RenderRequest request, RenderResponse response) throws IOException, PortletException {
+		
+		String publikUserId = getPublikID(request);
+		
+		// Recuperation et attribution des quartiers
+		List<AssetCategory> districtCategories = AssetVocabularyHelper.getAllDistrictsFromCity(CITY_NAME);
+		request.setAttribute("districtCategories", districtCategories);
+		
 		super.render(request, response);
+	}
+	
+	/**
+	 * Méthode exécutée lors d'un appel AJAX
+	 */
+	@Override
+	public void serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+			throws IOException, PortletException {
+		try {
+			String resourceID = resourceRequest.getResourceID();
+
+			if (resourceID.equals("changeDistrict")) {
+				
+			}
+			
+		} catch (Exception e) {
+			_log.error(e);
+		}
 	}
 	
 	/**
@@ -53,5 +98,7 @@ public class MapSearchAssetWebPortlet extends MVCPortlet {
 		
 		return SessionParamUtil.getString(originalRequest, "publik_internal_id");
 	}
+	
+	private final Log _log = LogFactoryUtil.getLog(this.getClass().getName());
 	
 }
