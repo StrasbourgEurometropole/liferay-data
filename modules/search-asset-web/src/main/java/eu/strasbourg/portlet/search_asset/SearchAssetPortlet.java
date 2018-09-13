8ce9a0817487f1d0d1c37539fd372b08b0a00ecc
alexandre.quere <alexandre.quere@sully-group.fr>
2018-09-13 12:26:37 +0200

    correctif merge branch

--- modules/search-asset-web/src/main/java/eu/strasbourg/portlet/search_asset/SearchAssetPortlet.java
+++ modules/search-asset-web/src/main/java/eu/strasbourg/portlet/search_asset/SearchAssetPortlet.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
