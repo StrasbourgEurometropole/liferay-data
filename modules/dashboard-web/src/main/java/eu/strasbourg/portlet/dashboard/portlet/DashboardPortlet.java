952bc46e7bdd342802283c32e711663e09c2ebf0
alexandre.quere <alexandre.quere@sully-group.fr>
2018-09-25 13:34:02 +0200

    poursuite developpement dashboard

--- modules/dashboard-web/src/main/java/eu/strasbourg/portlet/dashboard/portlet/DashboardPortlet.java
+++ modules/dashboard-web/src/main/java/eu/strasbourg/portlet/dashboard/portlet/DashboardPortlet.java
@@ -9,6 +9,7 @@ import com.liferay.portal.kernel.util.SessionParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import eu.strasbourg.service.oidc.model.PublikUser;
 import eu.strasbourg.service.oidc.service.PublikUserLocalServiceUtil;
+import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
 import org.osgi.service.component.annotations.Component;
 
 import javax.portlet.Portlet;
@@ -23,39 +24,40 @@ import java.io.IOException;
  * @author alexandre.quere
  */
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+        immediate = true,
+        property = {
+                "com.liferay.portlet.display-category=category.hidden",
+                "com.liferay.portlet.instanceable=true",
+                "javax.portlet.init-param.template-path=/",
+                "javax.portlet.init-param.view-template=/view.jsp",
+                "javax.portlet.name=" + StrasbourgPortletKeys.DASHBOARD_WEB,
+                "javax.portlet.resource-bundle=content.Language",
+                "javax.portlet.security-role-ref=power-user,user"
+        },
+        service = Portlet.class
 )
 public class DashboardPortlet extends MVCPortlet {
-	private final Log _log = LogFactoryUtil.getLog(this.getClass().getName());
+    private final Log _log = LogFactoryUtil.getLog(this.getClass().getName());
 
-	@Override
-	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
-		String publicId = getPublikID(renderRequest);
-		_log.info("je passe par le render");
-		if (Validator.isNotNull(publicId)){
-			PublikUser user = PublikUserLocalServiceUtil.getByPublikUserId(publicId);
-			renderRequest.setAttribute("hasUserSigned", Validator.isNotNull(user.getPactSignature()));
-			renderRequest.setAttribute("isUserloggedIn", true);
-		} else renderRequest.setAttribute("isUserloggedIn", false);
+    @Override
+    public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
+        String publicId = getPublikID(renderRequest);
+        _log.info("je passe par le render");
+        if (Validator.isNotNull(publicId)) {
+            PublikUser user = PublikUserLocalServiceUtil.getByPublikUserId(publicId);
+            renderRequest.setAttribute("hasUserSigned", Validator.isNotNull(user.getPactSignature()));
+            renderRequest.setAttribute("isUserloggedIn", true);
+        } else renderRequest.setAttribute("isUserloggedIn", false);
 
-		super.render(renderRequest, renderResponse);
-	}
+        super.render(renderRequest, renderResponse);
+    }
 
-	// Récupération du publik ID avec la session
-	private String getPublikID(PortletRequest resourceRequest) {
+    // Récupération du publik ID avec la session
+    private String getPublikID(PortletRequest resourceRequest) {
 
-		LiferayPortletRequest liferayPortletRequest = PortalUtil.getLiferayPortletRequest(resourceRequest);
-		HttpServletRequest originalRequest = liferayPortletRequest.getHttpServletRequest();
+        LiferayPortletRequest liferayPortletRequest = PortalUtil.getLiferayPortletRequest(resourceRequest);
+        HttpServletRequest originalRequest = liferayPortletRequest.getHttpServletRequest();
 
-		return SessionParamUtil.getString(originalRequest, "publik_internal_id");
-	}
+        return SessionParamUtil.getString(originalRequest, "publik_internal_id");
+    }
 }
--- modules/dashboard-web/src/main/resources/META-INF/resources/init.jsp
+++ modules/dashboard-web/src/main/resources/META-INF/resources/init.jsp
@@ -2,10 +2,15 @@
 
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
 
-<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
-taglib uri="http://liferay.com/tld/portlet" prefix="liferay-portlet" %><%@
-taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %><%@
-taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %>
+<%@ taglib uri="http://liferay.com/tld/asset" prefix="liferay-asset" %>
+<%@ taglib uri="http://liferay.com/tld/ddm" prefix="liferay-ddm" %>
+<%@ taglib uri="http://liferay.com/tld/portlet" prefix="liferay-portlet" %>
+<%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+<%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+<%@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 
 <liferay-theme:defineObjects />
 
