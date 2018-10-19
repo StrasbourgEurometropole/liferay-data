9cf11f55eb4a72352d2b53280cf24d2d92fb463b
alexandre.quere <alexandre.quere@sully-group.fr>
2018-10-19 11:17:16 +0200

    poursuite creation du statut des BP

--- modules/project/project-api/src/main/java/eu/strasbourg/service/project/constants/ParticiperCategories.java
+++ modules/project/project-api/src/main/java/eu/strasbourg/service/project/constants/ParticiperCategories.java
@@ -10,7 +10,18 @@ public enum ParticiperCategories {
     NEW("new"),
     IN_PROGRESS("in_progress"),
     SOON_FINISHED("soon_finished"),
-    FINISHED("finished");
+    FINISHED("finished"),
+    BP_SUBMITTED("submitted"),
+    BP_ACCEPTABLE("acceptable"),
+    BP_NON_ACCEPTABLE("non_acceptabled"),
+    BP_FEASIBLE("feasible"),
+    BP_NON_FEASIBLE("non_feasible"),
+    BP_LAUREAT("laureat"),
+    BP_NON_SELECTED("selected"),
+    BP_IN_PROGRESS("bp_in_progress"),
+    BP_REALIZED("realized"),
+    BP_SUSPENDED("suspended"),
+    BP_CANCELLED("cancelled");
 
     private String name;
 
