9a73f7b244922e495a4c70cdce1399d1631e5ac0
Cédric Henry <h.cedric.stylix@gmail.com>
2018-11-06 10:37:58 +0100

    project : rendre le choix d'un projet optionnel pour les participations

--- modules/project/project-service/src/main/java/eu/strasbourg/service/project/model/impl/ParticipationImpl.java
+++ modules/project/project-service/src/main/java/eu/strasbourg/service/project/model/impl/ParticipationImpl.java
@@ -278,8 +278,9 @@ public class ParticipationImpl extends ParticipationBaseImpl {
 	 */
 	@Override
 	public AssetCategory getProjectCategory() {
-		return AssetVocabularyHelper.getAssetEntryCategoriesByVocabulary(this.getAssetEntry(), VocabularyNames.PROJECT)
-				.get(0);
+		List<AssetCategory> listProject = AssetVocabularyHelper.getAssetEntryCategoriesByVocabulary(this.getAssetEntry(),
+                VocabularyNames.PROJECT);
+        return (listProject != null && !listProject.isEmpty()) ? listProject.get(0) : null;
 	}
 
 	/**
@@ -555,7 +556,7 @@ public class ParticipationImpl extends ParticipationBaseImpl {
 
 		// Champs : Description
 		jsonParticipation.put("consultationPlacesBody", HtmlUtil.stripHtml(HtmlUtil.escape(this.getConsultationPlacesBody())));
-
+		
 		// Champs : Dates
 		jsonParticipation.put("publicationDate", this.getPublicationDate());
 		jsonParticipation.put("expirationDate", this.getExpirationDate());
@@ -569,7 +570,7 @@ public class ParticipationImpl extends ParticipationBaseImpl {
 		AssetCategory projectCategory = this.getProjectCategory();
 		AssetCategory statusCategory = this.getParticipationStatusCategory();
 		AssetCategory typeCategory = this.getTypeCategory();
-
+		
 		jsonParticipation.put("districtsLabel", this.getDistrictLabel(Locale.FRENCH));
 		jsonParticipation.put("thematicsLabel", this.getThematicsLabel(Locale.FRENCH));
 		jsonParticipation.put("typeLabel", typeCategory != null ? typeCategory.getTitle(Locale.FRENCH) : "");
