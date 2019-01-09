ba5250710e0d5d77e1f4ac2d861da2d33741cbc2
romain.vergnais <romain.vergnais@sully-group.fr>
2019-01-09 11:06:50 +0100

    BuildService avant deploy

--- themes/plateforme-citoyenne-theme/src/js/strasbourg.js
+++ themes/plateforme-citoyenne-theme/src/js/strasbourg.js
@@ -15418,7 +15418,7 @@ function createInitiative(initiative){
                         '<figure role="group">' +
                             '<img src="' + initiative.authorImageURL + '" width="40" height="40" alt="Image de profil auteur"/>' +
                         '</figure>' +
-                        '<p>Participation publiée par :</p>' +
+                        '<p>Initiative publiée par :</p>' +
                         '<p><strong>' + initiative.author + '</strong></p>' +
                     '</div>' +
                     '<div class="pro-content-initiative">' +
@@ -15561,29 +15561,6 @@ $(document).ready(function () {
         $('.pro-modal').modal('hide');
     });
     
-});
-//méthode permettant de confirmer la fermeture de la popup en ouvrant une nouvelle popup.
-$("#closingButton").click(function(event){
-   event.preventDefault();
-   var temp = $(document.activeElement).parent().parent().parent().parent();
-   var zindex = $(".fade.in").css("z-index");
-   $("#modalQuitPetition").modal("show");
-   $("#modalQuitPetition").css('z-index',zindex+1)
-   $("#buttonConfirmQuit").click(function(event){
-        $("#modalQuitPetition").modal("hide");
-        temp.modal('hide');
-   });
-});
-$("#closingButton2").click(function(event){
-   event.preventDefault();
-   var temp = $(document.activeElement).parent().parent().parent().parent();
-   var zindex = $(".fade.in").css("z-index");
-   $("#modalQuitPetition").modal("show");
-   $("#modalQuitPetition").css('z-index',zindex+1)
-   $("#buttonConfirmQuit").click(function(event){
-        $("#modalQuitPetition").modal("hide");
-        temp.modal('hide');
-   });
 });
 if($('.pro-page-pacte').length > 0 || $('.pro-page-budget-participatif').length > 0){
 
