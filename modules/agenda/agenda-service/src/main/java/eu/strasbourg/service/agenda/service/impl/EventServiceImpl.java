da0e437f123ae5a6dad2f74a24924bc7c667741b
messagR <messagr@hotmail.com>
2019-02-04 15:00:04 +0100

    agenda: mantis n° 4770 -> ajout place dans event pour la méthode getEvent

--- modules/agenda/agenda-service/src/main/java/eu/strasbourg/service/agenda/service/impl/EventServiceImpl.java
+++ modules/agenda/agenda-service/src/main/java/eu/strasbourg/service/agenda/service/impl/EventServiceImpl.java
@@ -158,7 +158,13 @@ public class EventServiceImpl extends EventServiceBaseImpl {
         if (event == null || !event.isApproved()) {
             return JSONFactoryUtil.createJSONObject();
         }
-        return event.toJSON();
+        JSONObject eventJSON = event.toJSON();
+        if (Validator.isNotNull(event.getPlaceSIGId())) {
+            Place place = PlaceLocalServiceUtil.getPlaceBySIGId(event.getPlaceSIGId());
+            if(place != null)
+                eventJSON.put("place", place.toJSON());
+        }
+        return eventJSON;
     }
 
     @Override
