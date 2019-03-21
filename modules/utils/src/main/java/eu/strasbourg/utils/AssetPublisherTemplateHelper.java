4781b04a5105ee133d76c994c70e14cc2326a614
messagR <messagr@hotmail.com>
2019-03-21 16:39:25 +0100

    utils : création méthode de récupération de la taille d'une image pour opengraph

--- modules/utils/src/main/java/eu/strasbourg/utils/AssetPublisherTemplateHelper.java
+++ modules/utils/src/main/java/eu/strasbourg/utils/AssetPublisherTemplateHelper.java
@@ -1,5 +1,30 @@
 package eu.strasbourg.utils;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 public class AssetPublisherTemplateHelper {
+
+    /**
+     * Retourne la largeur/hauteur d'une image
+     */
+    public static String getImageWidthHeight(String filePath) {
+
+        String taille = "";
+        try {
+            Image img = ImageIO.read(new URL(filePath));
+            int width = ((BufferedImage) img).getWidth();
+            int height = ((BufferedImage) img).getHeight();
+            taille = width + "," + height;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return taille;
+    }
 
 }
