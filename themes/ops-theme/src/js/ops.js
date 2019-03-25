a60ba406e03682a888a71c1f7c48e7f871c29c0b
romain.vergnais <romain.vergnais@sully-group.fr>
2019-03-25 16:39:58 +0100

    update theme V5

--- themes/ops-theme/src/js/ops.js
+++ themes/ops-theme/src/js/ops.js
@@ -8673,6 +8673,51 @@ if($('.ops-wrapper-content-rh').length > 0) {
     }
 
 }
+// Récupère les valeurs dans la dropdown
+
+var thFacettes = {
+
+    states: {},
+
+    init: function(){
+
+        $('.ops-facette-checkbox').each(function(){
+            var facetteFilterEl = this;
+            var facetteName = $(this).find('input');
+
+            // console.log(facetteName);
+
+            setTimeout(function(){
+                $('a.selected',facetteFilterEl).off('click').on('click',function(e){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    $(this).html('');
+                    thFacettes.states[facetteName] = '';
+                });
+            },250);
+
+            thFacettes.states[facetteName] = '';
+
+            $('input',this).on('click', function (e) {
+                var newValue =  $(this).val();
+
+                $(this).parents('.ops-facette-checkbox').find('a.selected').html($(this).val());
+
+
+                $('.ops-facette-checkbox').removeClass('ops-open');
+
+                if(newValue != thFacettes.states[facetteName] && newValue != ''){
+                    thFacettes.states[facetteName] = newValue;
+                }else{
+                    thFacettes.states[facetteName] = '';
+                }
+            });
+        });
+
+    }
+};
+
+thFacettes.init();
 $('form select').selectric();
 
 
