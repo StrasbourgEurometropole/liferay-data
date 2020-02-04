97661ab96ec94c11b74e1aa27beeb1970a54bc63
HENRY Cédric <cedric.henry@sully-group.fr>
2020-02-04 18:18:18 +0100

    docker : début de mise en place des stack de migration et de prod/pre-prod

---
+++ docker/build-env.sh
@@ -0,0 +1,23 @@
+if [ -z "$1" ]
+then
+    echo "Please, enter a destination path in parameter"
+else
+    deployPath="$1"
+
+    echo "Create directory tree if not exits"
+    mkdir -p \
+        $deployPath/esdata \
+        $deployPath/liferay/deploy \
+        $deployPath/liferay/doclib \
+        $deployPath/mysql/lib \
+        $deployPath/mysql/log \
+        $deployPath/configs \
+        $deployPath/portainer
+    
+    echo "Change /esdata permission"
+    chmod g+rwx $deployPath/esdata
+    chgrp 1000 $deployPath/esdata
+    
+    echo "Copy config files"
+    cp -ru ./config/* $deployPath/config
+fi
