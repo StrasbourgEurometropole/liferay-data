2a8ce78823616b23f882e82e3269f149272f6f7c
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2020-04-24 19:46:53 +0200

    Council-web : squelette du JS dynamique

--- modules/council-web/src/main/resources/META-INF/resources/js/council-dynamic-view.js
+++ modules/council-web/src/main/resources/META-INF/resources/js/council-dynamic-view.js
@@ -5,7 +5,30 @@ $(document).ready(function(){
         officialId: officialConnectedId
       },
       function(obj) {
-        console.log(obj);
+        var delibAfficheId = document.getElementById("deliberationId");
+        var delibAfficheStatut = document.getElementById("stage");
+
+        var deliberationJSON = obj.deliberation;
+
+        // On modifie uniquement si l'id de la délib ou son statut a changé
+        if(delibAfficheId.value != deliberationJSON.deliberationId || delibAfficheStatut.value != deliberationJSON.stage) {
+            console.log("true");
+
+            if(deliberationJSON.stage == "Affichage en cours") {
+
+            } else if (deliberationJSON.stage == "Vote ouvert") {
+                var officialVote = obj.official.vote;
+
+                   //Procurations
+
+                if(officialVote) {
+
+                }
+
+            } else if(deliberationJSON.stage == "Adopté" || deliberationJSON.stage == "Rejeté" || deliberationJSON.stage == "Communiqué" ) {
+
+            }
+        }
       }
     );
 });
