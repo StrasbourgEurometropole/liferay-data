5085a0fc69c47cfe41c1d7322e868d1abe1fe915
HENRY Cédric <cedric.henry@sully-group.fr>
2020-04-27 18:47:54 +0200

    council-web & utils : mise en place des messages de connexion

--- modules/council-web/src/main/java/eu/strasbourg/portlet/council/display/context/CouncilDisplayContext.java
+++ modules/council-web/src/main/java/eu/strasbourg/portlet/council/display/context/CouncilDisplayContext.java
@@ -7,15 +7,26 @@ import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.SessionParamUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import eu.strasbourg.portlet.council.configuration.CouncilConfiguration;
+import eu.strasbourg.service.council.model.CouncilSession;
 import eu.strasbourg.service.council.model.Official;
+import eu.strasbourg.service.council.model.Procuration;
+import eu.strasbourg.service.council.service.CouncilSessionLocalServiceUtil;
 import eu.strasbourg.service.council.service.OfficialLocalServiceUtil;
-import eu.strasbourg.service.oidc.service.PublikUserLocalServiceUtil;
+import eu.strasbourg.service.council.service.ProcurationLocalServiceUtil;
+import eu.strasbourg.utils.LayoutHelper;
 
 import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 public class CouncilDisplayContext {
 
@@ -25,6 +36,7 @@ public class CouncilDisplayContext {
     private PortletPreferences preferences;
     private CouncilConfiguration configuration;
     private RenderRequest request;
+    private CouncilSession councilSession;
 
     /**
      * Constructeur
@@ -38,6 +50,7 @@ public class CouncilDisplayContext {
         } catch (PortalException e) {
             log.error(e);
         }
+        this.councilSession = null;
     }
 
     /**
@@ -51,11 +64,10 @@ public class CouncilDisplayContext {
     /**
      * Renvoi l'id de l'official Connecté (0 si pas un official de la BDD)
      */
+    @SuppressWarnings("unused")
     public long getOfficialId() {
         long officialId=0;
-        LiferayPortletRequest liferayPortletRequest = PortalUtil.getLiferayPortletRequest(request);
-        HttpServletRequest originalRequest = liferayPortletRequest.getHttpServletRequest();
-        String publikMail = SessionParamUtil.getString(originalRequest, "publik_email");
+        String publikMail = this.getPublikEmail();
 
         Official official = OfficialLocalServiceUtil.findByEmail(publikMail);
 
@@ -64,6 +76,126 @@ public class CouncilDisplayContext {
         }
 
         return officialId;
+    }
+
+    /**
+     * Renvoi la session en cours si elle existe
+     */
+    @SuppressWarnings("unused")
+    public CouncilSession getCouncilSession() {
+        if (this.councilSession == null) {
+            GregorianCalendar gc = new GregorianCalendar();
+            gc.setTime(new Date());
+            gc.set(Calendar.HOUR_OF_DAY, 0);
+            gc.set(Calendar.MINUTE, 0);
+            gc.set(Calendar.SECOND, 0);
+            gc.set(Calendar.MILLISECOND, 0);
+            List<CouncilSession> todayCouncils = CouncilSessionLocalServiceUtil.findByDate(gc.getTime());
+
+            if (todayCouncils.size() > 0)
+                this.councilSession = todayCouncils.get(0);
+        }
+        return this.councilSession;
+    }
+
+    /**
+     * Renvoi le type de la session du jour si elle existe
+     */
+    @SuppressWarnings("unused")
+    public String getCouncilSessionType() {
+        return this.getCouncilSession() != null ? this.councilSession.getType() : "";
+    }
+
+    /**
+     * Si l'utilisateur connecté est un utilisateur MonStrasbourg & co. sans lien avec les conseils
+     */
+    @SuppressWarnings("unused")
+    public boolean isConfirmedCouncilUser() {
+        return !isGuestUser()
+                && !this.isSessionNotAvailable()
+                && !this.isSimplePublikUser()
+                && !this.isOfficialTypeMismatchOrNotActive()
+                && !this.isOfficialNotedAbsent();
+    }
+
+    /**
+     * Si l'utilisateur n'est pas connecté (publik ou Liferay)
+     */
+    @SuppressWarnings("unused")
+    public boolean isGuestUser() {
+        return Validator.isNull(this.getPublikEmail());
+    }
+
+    /**
+     * Si l'utilisateur connecté est un utilisateur MonStrasbourg & co. sans lien avec les conseils
+     */
+    @SuppressWarnings("unused")
+    public boolean isSimplePublikUser() {
+        return this.getOfficialId() == 0 && Validator.isNotNull(this.getPublikEmail());
+    }
+
+    /**
+     * Si l'utilisateur connecté est un utilisateur MonStrasbourg & co. sans lien avec les conseils
+     */
+    @SuppressWarnings("unused")
+    public boolean isSessionNotAvailable() {
+        return this.getCouncilSession() == null;
+    }
+
+    /**
+     * Si les types de la session et de l'élu ne cole pas ou si l'élu est inactif
+     */
+    @SuppressWarnings("unused")
+    public boolean isOfficialTypeMismatchOrNotActive() {
+        boolean result = false;
+        Official currentOfficial = OfficialLocalServiceUtil.fetchOfficial(this.getOfficialId());
+        if (currentOfficial != null && this.getCouncilSession() != null) {
+            if ((this.getCouncilSession().isMinicipal() && currentOfficial.getIsMunicipal()
+                    || this.getCouncilSession().isEurometropolitan() && currentOfficial.getIsEurometropolitan())
+                    && currentOfficial.getIsActive())
+                result = false;
+            else
+                result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Si l'élu connecté est un élu noté comme absent pour la session
+     */
+    @SuppressWarnings("unused")
+    public boolean isOfficialNotedAbsent() {
+        boolean result = false;
+        if (this.getCouncilSession() != null) {
+            List<Procuration> procurations = ProcurationLocalServiceUtil.findByCouncilSessionId(
+                    this.getCouncilSession().getCouncilSessionId());
+            for (Procuration procuration : procurations) {
+                if (procuration.getOfficialUnavailableId() == this.getOfficialId())
+                    result = true;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Obtenir l'email de l'utilisateur
+     */
+    @SuppressWarnings("unused")
+    public String getPublikEmail() {
+        LiferayPortletRequest liferayPortletRequest = PortalUtil.getLiferayPortletRequest(this.request);
+        HttpServletRequest originalRequest = liferayPortletRequest.getHttpServletRequest();
+        return SessionParamUtil.getString(originalRequest, "publik_email");
+    }
+
+    /**
+     * Obtenir l'URL de connexion au service publik
+     */
+    @SuppressWarnings("unused")
+    public String getPublikLoginURL() throws MalformedURLException, UnsupportedEncodingException {
+        HttpServletRequest originalRequest = PortalUtil.getOriginalServletRequest(
+                PortalUtil.getHttpServletRequest(this.request));
+        return LayoutHelper.getPublikLoginURL(PortalUtil.getCurrentCompleteURL(originalRequest));
     }
 
     /**
--- modules/council-web/src/main/resources/META-INF/resources/council-dynamic-view.jsp
+++ modules/council-web/src/main/resources/META-INF/resources/council-dynamic-view.jsp
@@ -1,31 +1,102 @@
 <%@ include file="/council-init.jsp"%>
 
-<div class="council-flex council-web">
-	<div class="detail-delib seu-container">
+<c:choose>
 
-        <input type="hidden" id="deliberationId" value=""/>
-        <input type="hidden" id="stage" value=""/>
+    <%-- UTILISATEUR CONFIRME --%>
+    <c:when test = "${dc.isConfirmedCouncilUser()}">
 
-        <%@ include file="/templates/message.jsp"%>
-        <%@ include file="/templates/presentation-delib.jsp"%>
-        <%@ include file="/templates/result.jsp"%>
-        <%@ include file="/templates/vote-form.jsp"%>
+        <div class="council-flex council-web">
+            <div class="detail-delib seu-container">
 
-	</div>
-</div>
+                <input type="hidden" id="deliberationId" value=""/>
+                <input type="hidden" id="stage" value=""/>
 
-<!-- Définition des variables à partager avec le JS -->
-<liferay-util:html-top>
-	<script>
-	    var namespace = "<portlet:namespace />";
-		var groupId = ${dc.groupId};
-		var useSkypeView = ${dc.configuration.useSkypeView()};
-		var officialConnectedId = ${dc.officialId};
-	</script>
-</liferay-util:html-top>
+                <%-- INCLUSION DES TEMPLATES --%>
+                <%@ include file="/templates/message.jsp"%>
+                <%@ include file="/templates/presentation-delib.jsp"%>
+                <%@ include file="/templates/result.jsp"%>
+                <%@ include file="/templates/vote-form.jsp"%>
 
-<!-- Import des JS -->
-<liferay-util:html-bottom>
-    <script src="/o/councilweb/js/council-init-javascript.js" type="text/javascript"></script>
-    <script src="/o/councilweb/js/council-dynamic-view.js" type="text/javascript"></script>
-</liferay-util:html-bottom>
+            </div>
+        </div>
+
+        <%-- VARIABLES JS A PARTAGER --%>
+        <liferay-util:html-top>
+            <script>
+                var namespace = "<portlet:namespace />";
+                var groupId = ${dc.groupId};
+                var useSkypeView = ${dc.configuration.useSkypeView()};
+                var officialConnectedId = ${dc.officialId};
+            </script>
+        </liferay-util:html-top>
+
+        <%-- INCLUSIONS DES JS --%>
+        <liferay-util:html-bottom>
+            <script src="/o/councilweb/js/council-init-javascript.js" type="text/javascript"></script>
+            <script src="/o/councilweb/js/council-dynamic-view.js" type="text/javascript"></script>
+        </liferay-util:html-bottom>
+
+    </c:when>
+
+    <c:otherwise>
+        <div class="council-flex council-web">
+            <div class="detail-delib seu-container">
+                <h1 class="main-message-title">
+                    <liferay-ui:message key='welcome.on.platform'/>
+                </h1>
+
+                <c:choose>
+
+                    <%-- UTILISATEUR NON CONNECTE (LIFERAY OU PUBLIK) --%>
+                    <c:when test = "${dc.isGuestUser()}">
+                        <h2 class="main-message-text">
+                            <liferay-ui:message key='to.continue.please.connect'/>
+                        </h2>
+                        <div class="btn-validate-vote seu-btn-line"
+                                style="text-align: center; margin-top: 30px; margin-bottom: 30px;">
+                            <a href="${dc.getPublikLoginURL()}"
+                                    class="seu-btn-square seu-filled seu-second">
+                                <span class="seu-flexbox">
+                                    <span class="seu-btn-text">
+                                        <liferay-ui:message key='connect.to.mon.strasbourg'/>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </c:when>
+
+                    <%-- UTILISATEUR PUBLIK SIMPLE --%>
+                    <c:when test = "${dc.isSimplePublikUser()}">
+                        <h2 class="main-message-text">
+                            <liferay-ui:message key='site.for.officials.only'/>
+                        </h2>
+                    </c:when>
+
+                    <%-- TEST DE PRESENCE SESSION --%>
+                    <c:when test = "${dc.isSessionNotAvailable()}">
+                        <h2 class="main-message-text">
+                            <liferay-ui:message key='no.session.today'/>
+                        </h2>
+                    </c:when>
+
+                    <%-- TEST DU BON TYPE D'ELU --%>
+                    <c:when test = "${dc.isOfficialTypeMismatchOrNotActive()}">
+                        <h2 class="main-message-text">
+                            <liferay-ui:message key='session.reserved.to.${dc.getCouncilSessionType()}'/>
+                        </h2>
+                    </c:when>
+
+                    <%-- TEST D'ABSENCE DE L'ELU  --%>
+                    <c:when test = "${dc.isOfficialNotedAbsent()}">
+                        <h2 class="main-message-text">
+                            <liferay-ui:message key='noted.absent'/>
+                        </h2>
+                    </c:when>
+
+                </c:choose>
+
+            </div>
+        </div>
+    </c:otherwise>
+
+</c:choose>
