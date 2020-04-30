75405ccbdf258c3e5e4b80c3375460b90366dc30
HENRY Cédric <cedric.henry@sully-group.fr>
2020-04-30 15:24:27 +0200

    council-bo : implémentation d'un exemple d'export avec compression d'un répertoire local

--- modules/council-bo/src/main/java/eu/strasbourg/portlet/council/resource/ExportCouncilSessionResultsResourceCommand.java
+++ modules/council-bo/src/main/java/eu/strasbourg/portlet/council/resource/ExportCouncilSessionResultsResourceCommand.java
@@ -6,18 +6,15 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.util.ParamUtil;
 import eu.strasbourg.service.council.model.CouncilSession;
 import eu.strasbourg.service.council.service.CouncilSessionLocalService;
+import eu.strasbourg.utils.ZipHelper;
 import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
-import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.file.Files;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 @Component(
         immediate = true,
@@ -56,32 +53,14 @@ public class ExportCouncilSessionResultsResourceCommand implements MVCResourceCo
 
             try {
                 // Récupération de l'ouputStream de la réponse et création du zipOutputStream
-                OutputStream out = response.getPortletOutputStream();
-                ZipOutputStream zos = new ZipOutputStream(out);
+                OutputStream os = response.getPortletOutputStream();
 
-                // Récupération du fichier
-                File testFile1 = new File("C:/Users/cedric.henry/Documents/test 1.pdf");
-                File testFile2 = new File("C:/Users/cedric.henry/Documents/test 2.pdf");
-
-                // Ecriture du fichier 1 dans le zip
-                zos.putNextEntry(new ZipEntry(testFile1.getName()));
-                byte[] testFileContent1 = Files.readAllBytes(testFile1.toPath());
-                zos.write(testFileContent1, 0, testFileContent1.length);
-                zos.closeEntry();
-
-                // Ecriture du fichier 2 dans le zip
-                zos.putNextEntry(new ZipEntry(testFile2.getName()));
-                byte[] testFileContent2 = Files.readAllBytes(testFile2.toPath());
-                zos.write(testFileContent2, 0, testFileContent2.length);
-                zos.closeEntry();
-
-                // Envoie des données
-                zos.flush();
-                out.flush();
+                ZipHelper.zipDirectoryinOutputStream("C:/Users/cedric.henry/Documents/toZip", os);
+                
+                os.flush();
 
                 // Fermeture des outputStreams
-                zos.close();
-                out.flush();
+                os.close();
 
             } catch (IOException e) {
                 this.log.error("Une erreur est survenu lors de l'extraction de résultat de la session ", e);
