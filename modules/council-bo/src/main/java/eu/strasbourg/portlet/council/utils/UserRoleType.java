7ccd1a6d2b6e57613119afbce05c69bf22174908
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2020-06-05 18:12:22 +0200

    Méthode de récupération des catégories de Type conseil selon les rôles

--- modules/council-bo/src/main/java/eu/strasbourg/portlet/council/utils/UserRoleType.java
+++ modules/council-bo/src/main/java/eu/strasbourg/portlet/council/utils/UserRoleType.java
@@ -1,6 +1,7 @@
 package eu.strasbourg.portlet.council.utils;
 
 
+import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -9,6 +10,7 @@ import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import eu.strasbourg.service.council.model.Type;
 import eu.strasbourg.service.council.service.TypeLocalServiceUtil;
+import eu.strasbourg.utils.AssetVocabularyHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +41,37 @@ public class UserRoleType {
             }
         }
         return typesId;
+    }
+
+    /**
+     * Renvoie la liste des catégories de Type de conseil d'un Utilisateur en fonction de ses rôles
+     * @param themeDisplay
+     * @return
+     */
+    public static List<AssetCategory> typeCategoriesForUser(ThemeDisplay themeDisplay)  {
+        List<AssetCategory> typeCategories = new ArrayList<>();
+
+        // Si l'utilisateur est Admin liferay ou Admin Evote, il a tous les types Ids
+        if(themeDisplay.getPermissionChecker().isOmniadmin() || IsAdminEvote(themeDisplay)) {
+            for ( Type type : TypeLocalServiceUtil.getTypes(-1,-1)) {
+                AssetCategory category =  AssetVocabularyHelper.getCategory(type.getTitle(), themeDisplay.getScopeGroupId());
+                if(category != null) {
+                    typeCategories.add(category);
+                }
+            }
+        }
+        // Ou alors on regarde par rapport aux rôles
+        else {
+            for ( long roleId : themeDisplay.getUser().getRoleIds()) {
+                for ( Type type : TypeLocalServiceUtil.findByRoleId(roleId)) {
+                    AssetCategory category =  AssetVocabularyHelper.getCategory(type.getTitle(), themeDisplay.getScopeGroupId());
+                    if(category != null) {
+                        typeCategories.add(category);
+                    }
+                }
+            }
+        }
+        return typeCategories;
     }
 
     public static boolean IsAdminEvote(ThemeDisplay themeDisplay) {
