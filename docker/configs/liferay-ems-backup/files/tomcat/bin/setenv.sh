f9ec0befcb1a978c9bbc3d882a577bfd0f2779d7
HENRY Cédric <cedric.henry@sully-group.fr>
2020-08-31 16:56:39 +0200

    docker : réadaptation du gitignore

---
+++ docker/configs/liferay-ems-backup/files/tomcat/bin/setenv.sh
@@ -0,0 +1,1 @@
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Duser.timezone=Europe/Paris -Djava.locale.providers=JRE,COMPAT,CLDR -Xmx8192m -XX:MaxPermSize=512m -Djavamelody.storage-directory=/data/javamelody"
