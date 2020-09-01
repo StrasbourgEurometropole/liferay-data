30de2d497cde5ec4efc8a3766649faac6c9e12db
HENRY Cédric <cedric.henry@sully-group.fr>
2020-09-01 11:47:07 +0200

    docker : startup plus lisible

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,11 +1,12 @@
-# Export des variables d'environnement
+echo -e "\n\n 1# Mise à jour des variables d'environnements"
+cat .env
 export $(cat .env)
 
-# Construction de l'image Liferay
+echo -e "\n\n 2# Construction de l'image Liferay"
 docker image build --build-arg DIST_PATH_VERSION=$LFR_TAG_VERSION -t $REGISTRY_ADDRESS\liferay-ems:$LFR_TAG_VERSION ./images/liferay-ems
 
-# Push sur le registry de l'image Liferay
+echo -e "\n\n 3# Push sur le registry de l'image Liferay"
 docker image push $REGISTRY_ADDRESS\liferay-ems:$LFR_TAG_VERSION
 
-# Lancement de la stack EMS
+echo -e "\n\n 4# Lancement de la stack EMS"
 docker stack deploy -c docker-compose.yml ems-stack --with-registry-auth
