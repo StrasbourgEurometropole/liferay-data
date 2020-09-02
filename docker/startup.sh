e3d8426d05fdda5c5a0e5eaf1c753d302f64c0ae
HENRY Cédric <cedric.henry@sully-group.fr>
2020-09-02 14:53:27 +0200

    docker : incrémentation des labels du script startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -8,16 +8,16 @@ docker image build --build-arg -t $REGISTRY_ADDRESS\elasticsearch-ems ./images/e
 echo -e "\n\n \e[35m3# Push sur le registry de l'image ElasticSearch\e[0m"
 docker image push $REGISTRY_ADDRESS\elasticsearch-ems
 
-echo -e "\n\n \e[35m2# Construction de l'image Liferay\e[0m"
+echo -e "\n\n \e[35m4# Construction de l'image Liferay\e[0m"
 docker image build --build-arg DIST_PATH_VERSION=$LFR_TAG_VERSION -t $REGISTRY_ADDRESS\liferay-ems:$LFR_TAG_VERSION ./images/liferay-ems
 
-echo -e "\n\n \e[35m3# Push sur le registry de l'image Liferay\e[0m"
+echo -e "\n\n \e[35m5# Push sur le registry de l'image Liferay\e[0m"
 docker image push $REGISTRY_ADDRESS\liferay-ems:$LFR_TAG_VERSION
 
-echo -e "\n\n \e[35m4# Lancement de la stack EMS\e[0m"
+echo -e "\n\n \e[35m6# Lancement de la stack EMS\e[0m"
 docker stack deploy -c docker-compose.yml ems-stack --with-registry-auth
 
-echo -e "\n\n \e[35m4# Visualisation des services\e[0m"
+echo -e "\n\n \e[35m7# Visualisation des services\e[0m"
 docker service ls
 
 echo -e "\n\n"
