211405ebbc848f55ba4082ecca449584f7d74d54
Louis Politanski <louis.politanski@sully-group.fr>
2020-09-30 14:03:03 +0200

    Ajout de la conversion de PNGs volumineux

--- modules/dlfile-entry-local-service-override/src/main/java/eu/strasbourg/serviceOverride/DLFileEntry/DLFileEntryServiceOverride.java
+++ modules/dlfile-entry-local-service-override/src/main/java/eu/strasbourg/serviceOverride/DLFileEntry/DLFileEntryServiceOverride.java
@@ -15,6 +15,8 @@ import com.liferay.portal.kernel.image.ImageToolUtil;
 
 import org.osgi.service.component.annotations.Component;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.*;
 import java.util.Map;
@@ -57,19 +59,19 @@ public class DLFileEntryServiceOverride extends DLFileEntryLocalServiceWrapper {
 				int height = image.getHeight();
 				int width = image.getWidth();
 				float compressionRatio = (float) width * height / size;
-
+				float compressionQuality = 0.88f;
 				if (height > 1920 || width > 1920) {
 					// Scaling
 					image = ImageToolUtil.scale(image, 1920, 1920);
 					// Compression
-					ByteArrayOutputStream baos = compressImage(image);
+					ByteArrayOutputStream baos = compressImage(image, compressionQuality);
 					// Remplacement de l'image
 					is = new ByteArrayInputStream(baos.toByteArray());
 					size = baos.size();
 				}
 				else if (compressionRatio < 7) {
 					// Compression
-					ByteArrayOutputStream baos = compressImage(image);
+					ByteArrayOutputStream baos = compressImage(image, compressionQuality);
 					// Calcul du nouveau pourcentage de compression
 					float newCompressionRatio = 100 * (float)baos.size()/size;
 					// Remplacement de l'image si le pourcentage de compression est satisfaisant
@@ -84,17 +86,59 @@ public class DLFileEntryServiceOverride extends DLFileEntryLocalServiceWrapper {
 				e.printStackTrace();
 			}
 		}
+		else if (entry == null && mimeType.equals("image/png")) { //  && size > 500000
+			_log.error("Large PNG detected.");
+			try {
+				RenderedImage image = ImageIO.read(is);
+				int height = image.getHeight();
+				int width = image.getWidth();
+				float compressionQuality = 0.88f;
+				if (height > 1920 || width > 1920) {
+					// Scaling
+					image = ImageToolUtil.scale(image, 1920, 1920);
+				}
+
+				// Elimination de la transparence du PNG
+				BufferedImage newImg = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+				newImg.createGraphics().drawImage((Image) image, 0, 0, Color.BLACK, null);
+				image = newImg;
+
+				// Compression
+				ByteArrayOutputStream baos = compressImage(image, compressionQuality);
+				// Calcul du nouveau pourcentage de compression
+				float newCompressionRatio = 100 * (float)baos.size()/size;
+				// Remplacement de l'image si le pourcentage de compression est satisfaisant
+				if (newCompressionRatio < 90) {
+					_log.error("On remplace le PNG.");
+					is = new ByteArrayInputStream(baos.toByteArray());
+					size = baos.size();
+					mimeType = "image/jpeg";
+					// Modif extension fichier
+					if (sourceFileName.substring(sourceFileName.length()-4).equals(".png")) {
+						sourceFileName = sourceFileName.substring(0, sourceFileName.length()-4) + ".jpg";
+						_log.error("New filename:"+sourceFileName);
+					}
+				}
+				else {
+					_log.error("Compression insatisfaisante, on garde le PNG.");
+				}
+			}
+			catch (IOException e)
+			{ _log.error(e); } catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 
 		return super.addFileEntry(userId, groupId, repositoryId, folderId, sourceFileName, mimeType, title, description,
 				changeLog, fileEntryTypeId, ddmFormValuesMap, file, is, size, serviceContext);
 	}
 
-	private ByteArrayOutputStream compressImage(RenderedImage image) throws IOException {
+	private ByteArrayOutputStream compressImage(RenderedImage image, float quality) throws IOException {
 		// Configuration des parametres de compression
 		ImageWriter jpgWriter = ImageIO.getImageWritersByMIMEType("image/jpeg").next();
 		ImageWriteParam jpgWriteParam = jpgWriter.getDefaultWriteParam();
 		jpgWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-		jpgWriteParam.setCompressionQuality(0.88f);
+		jpgWriteParam.setCompressionQuality(quality);
 		// Configuration de l'output de compression
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		jpgWriter.setOutput(new MemoryCacheImageOutputStream(baos));
