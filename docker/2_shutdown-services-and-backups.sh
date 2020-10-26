bb60c93c653a16f5dc302f7bbb33e0f61cf37a93
HENRY Cédric <cedric.henry@sully-group.fr>
2020-10-26 09:53:49 +0100

    docker : ajout de la récupération des variables d'env avant le dump de la BDD

--- docker/2_shutdown-services-and-backups.sh
+++ docker/2_shutdown-services-and-backups.sh
@@ -1,5 +1,9 @@
-echo -e "\n\n \e[35m1# Arrêt des services\e[0m"
+echo -e "\n\n \e[35m1# Récupération des variables d'environnements\e[0m"
+cat .env
+export $(cat .env)
+
+echo -e "\n\n \e[35m2# Arrêt des services\e[0m"
 docker stack rm ems-stack
 
-echo -e "\n\n \e[35m2# Backup de la BDD\e[0m"
+echo -e "\n\n \e[35m3# Backup de la BDD\e[0m"
 sudo mysqldump --opt $MYSQL_DB > $DB_BACKUPS_PATH/$MYSQL_DB_$(date +%Y%m%d-%H%M).sql
