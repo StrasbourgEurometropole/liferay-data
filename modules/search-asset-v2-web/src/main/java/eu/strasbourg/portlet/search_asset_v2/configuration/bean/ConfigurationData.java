79c5336a78853e0a295eee1baa7e430ad41f7ce5
HENRY Cédric <cedric.henry@sully-group.fr>
2020-11-02 15:03:26 +0100

    search-asset-v2 : initialisation du bean de configuration

---
+++ modules/search-asset-v2-web/src/main/java/eu/strasbourg/portlet/search_asset_v2/configuration/bean/ConfigurationData.java
@@ -0,0 +1,40 @@
+package eu.strasbourg.portlet.search_asset_v2.configuration.bean;
+
+import eu.strasbourg.portlet.search_asset_v2.configuration.SearchAssetConfiguration;
+
+import javax.portlet.ActionRequest;
+
+public class ConfigurationData {
+
+    /** Contextes de récupération des données */
+    private ActionRequest request;
+    private SearchAssetConfiguration configuration;
+
+    /** Donnée */
+    
+
+    public ConfigurationData(ActionRequest request) {
+        this.request = request;
+        this.initDataFromRequest();
+    }
+
+    public ConfigurationData(SearchAssetConfiguration configuration) {
+        this.configuration = configuration;
+        this.initDataFromConfiguration();
+    }
+
+    private void initDataFromRequest() {
+        // TODO Récupération des données de la requête
+    }
+
+    private void initDataFromConfiguration() {
+        // TODO Récupération des données des portlet preferences de la configuration
+    }
+
+    public boolean validate() {
+        boolean result = true;
+        // TODO Validation des données issues de la requête
+        return result;
+    }
+
+}
