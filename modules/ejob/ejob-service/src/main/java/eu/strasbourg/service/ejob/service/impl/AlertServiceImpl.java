4b60e2fe0ed29155f66ed6da653269d9393a60df
HENRY Cédric <cedric.henry@sully-group.fr>
2020-11-16 16:00:21 +0100

    ejob : empècher la création d'alerte sans nom

--- modules/ejob/ejob-service/src/main/java/eu/strasbourg/service/ejob/service/impl/AlertServiceImpl.java
+++ modules/ejob/ejob-service/src/main/java/eu/strasbourg/service/ejob/service/impl/AlertServiceImpl.java
@@ -44,6 +44,9 @@ import java.util.List;
  */
 public class AlertServiceImpl extends AlertServiceBaseImpl {
 
+	public static final String ERROR_ALERT_NEEDS_NAME = "alertNeedsName";
+	public static final int NAME_SIZE = 75;
+
 	/*
 	 * NOTE FOR DEVELOPERS:
 	 *
@@ -64,14 +67,17 @@ public class AlertServiceImpl extends AlertServiceBaseImpl {
 			try {
 				ServiceContext sc = ServiceContextFactory.getInstance(request);
 
-				// on vérifi si une alerte du même nom pour cet utilisateur n'existe pas déjà
+				// on vérifie si une alerte du même nom pour cet utilisateur n'existe pas déjà
 				List<Alert> alerts = this.alertLocalService.findByPublikUserId(id);
 				if(alerts.size() > 0 && alerts.stream().anyMatch(a -> StringHelper.compareIgnoringAccentuation(a.getName().toLowerCase(), name.toLowerCase())))
 					return error("alreadyExist");
+				// on vérifie la taille du nom de l'alerte
+				if (name.length() < 1)
+					return error(ERROR_ALERT_NEEDS_NAME);
 
 				// Création de l'objet
 				// Limite la taille du nom à la taille du champ en base
-				String resizedName = name.length() > 75 ? name.substring(0, 74) : name;
+				String resizedName = name.length() > NAME_SIZE ? name.substring(0, NAME_SIZE - 1) : name;
 				Alert alert = this.alertLocalService.createAlert(sc);
 				alert.setName(resizedName);
 				alert.setKeyWord(keyword);
