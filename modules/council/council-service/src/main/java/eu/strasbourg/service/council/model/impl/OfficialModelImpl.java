f0d6e9b05ee479fcc678008bc440d8ddd67dd2b8
HENRY Cédric <cedric.henry@sully-group.fr>
2020-11-27 15:13:29 +0100

    mantis #5450 : E-Vote : problème de récupération du conseil du jour après changement de date --> supprimer la gestion du cache pour les entités E-Vote

--- modules/council/council-service/src/main/java/eu/strasbourg/service/council/model/impl/OfficialModelImpl.java
+++ modules/council/council-service/src/main/java/eu/strasbourg/service/council/model/impl/OfficialModelImpl.java
@@ -135,12 +135,12 @@ public class OfficialModelImpl
 	public static final boolean ENTITY_CACHE_ENABLED = GetterUtil.getBoolean(
 		eu.strasbourg.service.council.service.util.ServiceProps.get(
 			"value.object.entity.cache.enabled.eu.strasbourg.service.council.model.Official"),
-		true);
+		false);
 
 	public static final boolean FINDER_CACHE_ENABLED = GetterUtil.getBoolean(
 		eu.strasbourg.service.council.service.util.ServiceProps.get(
 			"value.object.finder.cache.enabled.eu.strasbourg.service.council.model.Official"),
-		true);
+		false);
 
 	public static final boolean COLUMN_BITMASK_ENABLED = GetterUtil.getBoolean(
 		eu.strasbourg.service.council.service.util.ServiceProps.get(
