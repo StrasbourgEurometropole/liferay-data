57413b0a70025ab435bdd1ac4e8a927e019b14a2
HENRY Cédric <cedric.henry@sully-group.fr>
2021-03-08 09:27:54 +0100

    gitlab #45 : définition du service Google Analystics via GTag et non GA

--- themes/aider-theme/custom/js/override/tarteaucitron.custom.js
+++ themes/aider-theme/custom/js/override/tarteaucitron.custom.js
@@ -1,4 +1,4 @@
-// Service Google Analytics (ga.js)
-tarteaucitron.user.gajsUa = 'G-RQF81GVH8X';
-tarteaucitron.user.gajsMore = function () { /* add here your optionnal _ga.push() */ };
-(tarteaucitron.job = tarteaucitron.job || []).push('gajs');
+// Service Google Analytics (gtag.js)
+tarteaucitron.user.gtagUa = 'G-RQF81GVH8X';
+tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
+(tarteaucitron.job = tarteaucitron.job || []).push('gtag');
