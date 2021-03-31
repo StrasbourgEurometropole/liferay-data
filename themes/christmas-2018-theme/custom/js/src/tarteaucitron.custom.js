d3221383f1b53c6bab86a523f67b69a66cf13ac7
messagr <messagr@hotmail.com>
2021-03-31 17:22:23 +0200

    noel : ajout de facebook pixel

--- themes/christmas-2018-theme/custom/js/src/tarteaucitron.custom.js
+++ themes/christmas-2018-theme/custom/js/src/tarteaucitron.custom.js
@@ -74,7 +74,19 @@ tarteaucitron.init({
 
 });
 
-// Service Google Analytics (gtag.js)
-tarteaucitron.user.gtagUa = 'G-RQF81GVH8X';
-tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
-(tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+// Service Google Analytics (universal)
+tarteaucitron.user.analyticsUa = 'UA-19910890-1'; // Ne pas oublier de vider le champ Identifiant Google Analytics dans les paramètres avancés du site
+tarteaucitron.user.analyticsMore = function () {  };
+(tarteaucitron.job = tarteaucitron.job || []).push('analytics');
+
+// Service Youtube
+//<div class="youtube_player" videoID="video_id" width="width" height="height" theme="theme (dark | light)" rel="rel (1 | 0)" controls="controls (1 | 0)" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" mute="mute (0 | 1)"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('youtube');
+
+// Service Daylimotion
+//<div class="dailymotion_player" videoID="video_id" width="width" height="height" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" embedType="embedType (video | playlist)"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('dailymotion');
+
+// Service Facebook Pixel
+tarteaucitron.user.facebookpixelId = '1023621684487107'; tarteaucitron.user.facebookpixelMore = function () { /* add here your optionnal facebook pixel function */ };
+(tarteaucitron.job = tarteaucitron.job || []).push('facebookpixel');
