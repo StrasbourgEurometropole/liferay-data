2555686fada0fdb2cb32b740fe1955ac798fe2b3
messagr <messagr@hotmail.com>
2021-03-31 17:05:18 +0200

    ond : mise en place des services

--- themes/ond-theme/src/js/tarteaucitron.custom.js
+++ themes/ond-theme/src/js/tarteaucitron.custom.js
@@ -74,7 +74,18 @@ tarteaucitron.init({
 
 });
 
-// Service Google Analytics (gtag.js)
-tarteaucitron.user.gtagUa = 'G-RQF81GVH8X';
-tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
-(tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+// Service Google Analytics (gtag.js) [for multiple UA]
+tarteaucitron.user.multiplegtagUa = ['UA-49889878-1'];
+(tarteaucitron.job = tarteaucitron.job || []).push('multiplegtag');
+
+// Service Youtube
+//<div class="youtube_player" videoID="video_id" width="width" height="height" theme="theme (dark | light)" rel="rel (1 | 0)" controls="controls (1 | 0)" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" mute="mute (0 | 1)"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('youtube');
+
+// Service Daylimotion
+//<div class="dailymotion_player" videoID="video_id" width="width" height="height" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" embedType="embedType (video | playlist)"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('dailymotion');
+
+// Service Calaméo
+//<div class="calameo-canvas" data-id="bkcode" width="width" height="height"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('calameo');
