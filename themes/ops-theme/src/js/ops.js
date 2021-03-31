4ce74fb384a7ed9a7bb46ee935560065f1a0792f
HENRY Cédric <cedric.henry@sully-group.fr>
2021-03-31 18:56:56 +0200

    ajout des services tarteaucitron sur OPS

--- themes/ops-theme/src/js/ops.js
+++ themes/ops-theme/src/js/ops.js
@@ -5095,18 +5095,13 @@ tarteaucitron.init({
     "mandatory": true, /* Show a message about mandatory cookies */
 
 });
-
-// Service Google Analytics (gtag.js)
-/*tarteaucitron.user.gtagUa = 'UA-32590640-1';
-tarteaucitron.user.gtagMore = function () { };
-(tarteaucitron.job = tarteaucitron.job || []).push('gtag');*/
-
 // Service Google Analytics (universal)
 tarteaucitron.user.analyticsUa = 'UA-32590640-1'; // Ne pas oublier de vider le champ Identifiant Google Analytics dans les paramètres avancés du site
 tarteaucitron.user.analyticsMore = function () {  };
 (tarteaucitron.job = tarteaucitron.job || []).push('analytics');
 
 // Service Youtube
+//<div class="youtube_player" videoID="video_id" width="width" height="height" theme="theme (dark | light)" rel="rel (1 | 0)" controls="controls (1 | 0)" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" mute="mute (0 | 1)"></div>
 (tarteaucitron.job = tarteaucitron.job || []).push('youtube');
 
 function CitronVideoHtml(id, plateforme, autoplay, mute) {
