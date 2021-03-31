d656852a171c86b7a2e033b7f2bbf8888b03a050
HENRY Cédric <cedric.henry@sully-group.fr>
2021-03-31 18:56:24 +0200

    ajout des services tarteaucitron sur Vidéo

--- themes/videos-theme/src/js/tarteaucitron.custom.js
+++ themes/videos-theme/src/js/tarteaucitron.custom.js
@@ -75,8 +75,18 @@ tarteaucitron.init({
 });
 
 // Service Google Analytics (gtag.js)
-tarteaucitron.user.gtagUa = 'G-RQF81GVH8X';
+tarteaucitron.user.gtagUa = 'UA-33301756-4';
 tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
 (tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+
+// Service Youtube
+//<div class="youtube_player" videoID="video_id" width="width" height="height" theme="theme (dark | light)" rel="rel (1 | 0)" controls="controls (1 | 0)" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" mute="mute (0 | 1)"></div>
 (tarteaucitron.job = tarteaucitron.job || []).push('youtube');
-(tarteaucitron.job = tarteaucitron.job || []).push('facebook');
+
+// Service Daylimotion
+//<div class="dailymotion_player" videoID="video_id" width="width" height="height" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" embedType="embedType (video | playlist)"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('dailymotion');
+
+// Service Viméo
+// <div class="vimeo_player" videoID="video_id" width="width" height="height"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('vimeo');
