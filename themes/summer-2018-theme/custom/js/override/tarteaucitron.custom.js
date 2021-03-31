3f6e422225d045c2aa3f4f71a53e6abf5eec33ec
HENRY Cédric <cedric.henry@sully-group.fr>
2021-03-31 18:38:58 +0200

    ajout des services tarteaucitron sur Été

--- themes/summer-2018-theme/custom/js/override/tarteaucitron.custom.js
+++ themes/summer-2018-theme/custom/js/override/tarteaucitron.custom.js
@@ -75,6 +75,10 @@ tarteaucitron.init({
 });
 
 // Service Google Analytics (gtag.js)
-tarteaucitron.user.gtagUa = 'G-RQF81GVH8X';
+tarteaucitron.user.gtagUa = 'UA-16973980-1';
 tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
 (tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+
+// Service reCAPTCHA
+tarteaucitron.user.recaptchaapi = '6LeGvPkSAAAAAFcWK3UVF6zPlAxGUKBq3xaR5Xi6';
+(tarteaucitron.job = tarteaucitron.job || []).push('recaptcha');
