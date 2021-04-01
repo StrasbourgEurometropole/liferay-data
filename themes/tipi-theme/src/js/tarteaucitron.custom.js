e2c1c7f62eaa25c7a0a14bcc0810f2c0d39a8742
messagr <messagr@hotmail.com>
2021-04-01 17:02:37 +0200

    tipi : mise en place des services

--- themes/tipi-theme/src/js/tarteaucitron.custom.js
+++ themes/tipi-theme/src/js/tarteaucitron.custom.js
@@ -2,3 +2,6 @@
 tarteaucitron.user.gtagUa = 'UA-33301756-8';
 tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
 (tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+
+// Service reCAPTCHA
+(tarteaucitron.job = tarteaucitron.job || []).push('recaptcha');
