aa99b5b68874e2487d0b64497f8b663501906788
messagr <messagr@hotmail.com>
2021-04-01 10:24:35 +0200

    mon-strasbourg : gitlab #52 -> initialisation tarteaucitron

---
+++ themes/monstrasbourg-theme/custom/override/tarteaucitron.custom.js
@@ -0,0 +1,12 @@
+// Service Google Analytics (universal)
+tarteaucitron.user.analyticsUa = 'UA-33301756-13'; // Ne pas oublier de vider le champ Identifiant Google Analytics dans les paramètres avancés du site
+tarteaucitron.user.analyticsMore = function () {  };
+(tarteaucitron.job = tarteaucitron.job || []).push('analytics');
+
+// Service Youtube
+//<div class="youtube_player" videoID="video_id" width="width" height="height" theme="theme (dark | light)" rel="rel (1 | 0)" controls="controls (1 | 0)" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" mute="mute (0 | 1)"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('youtube');
+
+// Service Daylimotion
+//<div class="dailymotion_player" videoID="video_id" width="width" height="height" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" embedType="embedType (video | playlist)"></div>
+(tarteaucitron.job = tarteaucitron.job || []).push('dailymotion');
