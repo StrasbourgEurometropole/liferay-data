e7116328d7629f8fa58e339d3bea07141eaf1b5a
messagr <messagr@hotmail.com>
2021-04-01 16:57:26 +0200

    musées : mise en place des services

--- themes/museum-theme/src/js/tarteaucitron.custom.js
+++ themes/museum-theme/src/js/tarteaucitron.custom.js
@@ -2,3 +2,6 @@
 tarteaucitron.user.analyticsUa = 'UA-12946284-2'; // Ne pas oublier de vider le champ Identifiant Google Analytics dans les paramètres avancés du site
 tarteaucitron.user.analyticsMore = function () {  };
 (tarteaucitron.job = tarteaucitron.job || []).push('analytics');
+
+// Service reCAPTCHA
+(tarteaucitron.job = tarteaucitron.job || []).push('recaptcha');
