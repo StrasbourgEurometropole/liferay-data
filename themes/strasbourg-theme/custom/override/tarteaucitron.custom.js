f8ccd1cd95f10356581f6943c491ce1ede799270
messagr <messagr@hotmail.com>
2021-04-01 14:23:21 +0200

    strasbourg : mise en place des services

--- themes/strasbourg-theme/custom/override/tarteaucitron.custom.js
+++ themes/strasbourg-theme/custom/override/tarteaucitron.custom.js
@@ -1,5 +1,6 @@
 // Service Google Analytics (universal)
-tarteaucitron.user.analyticsUa = 'UA-33301756-13'; // Ne pas oublier de vider le champ Identifiant Google Analytics dans les paramètres avancés du site
+// tarteaucitron.user.analyticsUa = 'UA-33301756-13'; -> français
+// tarteaucitron.user.analyticsUa = 'UA-36152381-1'; -> int
 tarteaucitron.user.analyticsMore = function () {  };
 (tarteaucitron.job = tarteaucitron.job || []).push('analytics');
 
@@ -10,3 +11,39 @@ tarteaucitron.user.analyticsMore = function () {  };
 // Service Daylimotion
 //<div class="dailymotion_player" videoID="video_id" width="width" height="height" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" embedType="embedType (video | playlist)"></div>
 (tarteaucitron.job = tarteaucitron.job || []).push('dailymotion');
+
+// Service reCAPTCHA
+(tarteaucitron.job = tarteaucitron.job || []).push('recaptcha');
+
+// Service custom : Creacast
+tarteaucitron.services.iframecreacast = {
+    "key": "iframecreacast",
+    "type": "video",
+    "name": "Creacast",
+    "uri": "",
+    "needConsent": true,
+    "cookies": ['__utm*'],
+    "js": function () {
+      "use strict";
+      tarteaucitron.fallback(['tac_iframecreacast'], function (x) {
+          var video_url = x.getAttribute("videoURL"),
+              video_frame;
+  
+          if (video_url === undefined) {
+              return "";
+          }
+          video_frame = '<iframe  scrolling="yes" height="1000px" frameborder="0" allowfullscreen  src="' + video_url + '"></iframe>';
+          return video_frame;
+      });
+    },
+    "fallback": function () {
+      "use strict";
+      var id = 'iframecreacast';
+      tarteaucitron.fallback(['tac_iframecreacast'], function (elem) {
+          elem.style.width = elem.getAttribute('width') + 'px';
+          elem.style.height = elem.getAttribute('height') + 'px';
+          return tarteaucitron.engage(id);
+      });
+    }
+};
+  (tarteaucitron.job = tarteaucitron.job || []).push('iframecreacast');
