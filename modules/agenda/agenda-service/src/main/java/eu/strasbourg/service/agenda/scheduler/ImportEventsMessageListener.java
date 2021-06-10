b41b57b150e962a0a117668811a1324716b442e9
Quentin Mayer <qmayer.sullygroup@gmail.com>
2021-06-10 14:31:08 +0200

    Correctif scheduler comme avant

--- modules/agenda/agenda-service/src/main/java/eu/strasbourg/service/agenda/scheduler/ImportEventsMessageListener.java
+++ modules/agenda/agenda-service/src/main/java/eu/strasbourg/service/agenda/scheduler/ImportEventsMessageListener.java
@@ -38,26 +38,15 @@ public class ImportEventsMessageListener
 		Date fiveMinutesFromNow = now.getTime();
 
 		// Création du trigger "Tous les jours à 4h"
-		Trigger firstTrigger = _triggerFactory.createTrigger(
+		Trigger trigger = _triggerFactory.createTrigger(
 				listenerClass, listenerClass, fiveMinutesFromNow, null,
 				"0 0 4 * * ?");
 
-		SchedulerEntry firstSchedulerEntry = new SchedulerEntryImpl(
-				listenerClass, firstTrigger);
+		SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
+				listenerClass, trigger);
 
 		_schedulerEngineHelper.register(
-				this, firstSchedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
-
-		// Création du trigger "Tous les jours à 13h30"
-		Trigger secondTrigger = _triggerFactory.createTrigger(
-				listenerClass, listenerClass, fiveMinutesFromNow, null,
-				"0 30 13 * * ?");
-
-		SchedulerEntry secondSchedulerEntry = new SchedulerEntryImpl(
-				listenerClass, secondTrigger);
-
-		_schedulerEngineHelper.register(
-				this, secondSchedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
+				this, schedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
 
 		log.info("Finish import events scheduler activation");
 	}
