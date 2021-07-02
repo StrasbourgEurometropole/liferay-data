db30849d18ca81ecde2b198521fcf0fe72954ab8
messagr <messagr@hotmail.com>
2021-07-02 11:57:23 +0200

    all : mantis #5670 -> Bug du captcha du formulaire de contact du site de l'été

--- themes/summer-2018-theme/custom/js/override/tarteaucitron.custom.js
+++ themes/summer-2018-theme/custom/js/override/tarteaucitron.custom.js
@@ -4,5 +4,4 @@ tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */
 (tarteaucitron.job = tarteaucitron.job || []).push('gtag');
 
 // Service reCAPTCHA
-tarteaucitron.user.recaptchaapi = '6LeGvPkSAAAAAFcWK3UVF6zPlAxGUKBq3xaR5Xi6';
 (tarteaucitron.job = tarteaucitron.job || []).push('recaptcha_ems');
