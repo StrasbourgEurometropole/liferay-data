d01efda2f2f771aad40086149ec4ada0d3c62bef
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2021-08-05 21:23:57 +0200

    #COuncil : Nettoyage JS de l'édit de conseil

--- modules/council-bo/src/main/resources/META-INF/resources/js/council-bo-edit-council-session.js
+++ modules/council-bo/src/main/resources/META-INF/resources/js/council-bo-edit-council-session.js
@@ -1,46 +1,13 @@
 var namespace = '_eu_strasbourg_portlet_council_CouncilBOPortlet_';
 
-/** Lors de la sélection d'un type de session **/
-$('#' + namespace + 'council-type').change(function() {
-    hideAllOfficials();
-    showSelectedTypeOfficials(this.value);
-});
-
-/** Lors du chargement de la page **/
-$(document).ready(function() {
-    hideAllOfficials();
-    showSelectedTypeOfficials($('#' + namespace + 'council-type').val());
-});
-
-/** Cache tous les élus **/
-function hideAllOfficials() {
-    $('tr[data-council-types]').hide();
-}
-
-/** Affiche les élus du type sélectionné **/
-function showSelectedTypeOfficials(type) {
-    $('tr[data-council-types*=' + type + ']').show();
-}
-
-/** Lors d'un check/unchecked d'une absence **/
-$('input[name$=-isAbsent]').on('change',function(){
-    var val = $(this).is(':checked');
-    var officialId = $(this).attr("name").replace(namespace,'').replace("-isAbsent",'');;
-    if (val)
-        $("input[name=" + namespace + "" + officialId + "-officialVoters]").prop('disabled', false);
-    else
-        $("input[name=" + namespace + "" + officialId + "-officialVoters]").prop('disabled', true);
-});
-
 jQuery(function() {
     /** Autocomplete des élus */
-    var typeCouncilSession = document.getElementById(namespace+"typeCouncilSessionHidden").value;
     var options = {
         type : "POST",
         serviceUrl : "/api/jsonws/council.official/get-official-by-full-name-and-type/",
         params : {
             fullName : '[fullName]',
-            type : typeCouncilSession,
+            type : "",
             removedOfficialId : 0,
             groupId : currentGroupId,
             p_auth: Liferay.authToken
