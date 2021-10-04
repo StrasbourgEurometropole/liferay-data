67905bdb76c845ea33b30360c9a8b6f798355334
Quentin Mayer <qmayer.sullygroup@gmail.com>
2021-10-04 17:03:00 +0200

    Gitlab #108 : BO si des sous catégories ont été enregistrés sans la parente, enregistré la parente + API ADD s'il fait partie de ce qu'il y a dans le BO mais pas dans la liste renvoyé par l'API

--- modules/csmap/csmap-bo/csmap-bo-place/src/main/java/eu/strasbourg/portlet/place/csmap/action/SaveCsmapPlaceCategoriesActionCommand.java
+++ modules/csmap/csmap-bo/csmap-bo-place/src/main/java/eu/strasbourg/portlet/place/csmap/action/SaveCsmapPlaceCategoriesActionCommand.java
@@ -1,6 +1,9 @@
 package eu.strasbourg.portlet.place.csmap.action;
 
+import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.asset.kernel.service.AssetCategoryLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
@@ -16,6 +19,9 @@ import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component(
         immediate = true,
@@ -40,11 +46,24 @@ public class SaveCsmapPlaceCategoriesActionCommand extends BaseMVCActionCommand 
 
         StringBuilder placeTypes = new StringBuilder();
         long[] placeTypesIds = ParamUtil.getLongValues(request, "Vocabulary_" + getTypeVocabularyId());
+        List<Long> placeTypesIdsList = Arrays.stream(placeTypesIds).boxed().collect(Collectors.toList());
         for (long placeTypesId : placeTypesIds) {
             if (placeTypes.toString().equals("")) {
                 placeTypes = new StringBuilder(String.valueOf(placeTypesId));
             } else {
                 placeTypes.append(",").append(placeTypesId);
+            }
+            try {
+                AssetCategory placeTypeCategory = _assetCategoryLocalService.getAssetCategory(placeTypesId);
+                AssetCategory parentCategory = placeTypeCategory.getParentCategory();
+                if (Validator.isNotNull(parentCategory)) {
+                    long parentId = parentCategory.getCategoryId();
+                    if (!placeTypesIdsList.contains(parentId)) {
+                        placeTypes.append(",").append(parentId);
+                    }
+                }
+            } catch (PortalException e) {
+                e.printStackTrace();
             }
         }
         placeCategories.setCategoriesIds(placeTypes.toString());
@@ -53,17 +72,24 @@ public class SaveCsmapPlaceCategoriesActionCommand extends BaseMVCActionCommand 
     }
 
     private PlaceCategoriesLocalService _placeCategoriesLocalService;
+    private AssetCategoryLocalService _assetCategoryLocalService;
 
     @Reference(unbind = "-")
-    protected void setAgendaExportLocalService(PlaceCategoriesLocalService placeCategoriesLocalService) {
+    protected void setPlaceCategoriesLocalService(PlaceCategoriesLocalService placeCategoriesLocalService) {
 
         _placeCategoriesLocalService = placeCategoriesLocalService;
     }
 
-    private String getTypeVocabularyId(){
+    @Reference(unbind = "-")
+    protected void setAssetCategoryLocalService(AssetCategoryLocalService assetCategoryLocalService) {
+
+        _assetCategoryLocalService = assetCategoryLocalService;
+    }
+
+    private String getTypeVocabularyId() {
         try {
             AssetVocabulary type = AssetVocabularyHelper.getGlobalVocabulary(VocabularyNames.PLACE_TYPE);
-            if(Validator.isNotNull(type))
+            if (Validator.isNotNull(type))
                 return String.valueOf(type.getVocabularyId());
         } catch (PortalException e) {
             e.printStackTrace();
