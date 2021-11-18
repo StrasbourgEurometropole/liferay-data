6c11e07b0c00bb99e52a6ff82c41452917aa56b7
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2021-11-18 20:15:37 +0100

    Revert : "Merge remote-tracking branch 'origin/work/mantis-5636' into release/v3.24.x"

--- themes/strasbourg-theme/custom/02-particules/02-modules/forms/liferay-form/liferay-form.js
+++
@@ -1,4 +0,0 @@
-var validateForms = document.getElementsByClassName("ddm-form-success-page");
-if(validateForms.length > 0 ){
-    $('html,body').animate({scrollTop: validateForms[0].offsetParent.offsetTop + 100}, 'slow');
-}
