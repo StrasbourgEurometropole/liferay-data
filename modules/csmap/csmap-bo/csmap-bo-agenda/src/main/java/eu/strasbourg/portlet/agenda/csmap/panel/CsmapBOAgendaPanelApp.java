cd2402ba9b9cb4659ad4ed01cf056f6113d37ae1
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2021-12-12 13:05:07 +0100

    #CSMap BO : Remise des BO dans "Contenu" + changement de nom

--- modules/csmap/csmap-bo/csmap-bo-agenda/src/main/java/eu/strasbourg/portlet/agenda/csmap/panel/CsmapBOAgendaPanelApp.java
+++ modules/csmap/csmap-bo/csmap-bo-agenda/src/main/java/eu/strasbourg/portlet/agenda/csmap/panel/CsmapBOAgendaPanelApp.java
@@ -2,6 +2,7 @@ package eu.strasbourg.portlet.agenda.csmap.panel;
 
 import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.portal.kernel.model.Portlet;
 import eu.strasbourg.panel.category.csmap.constants.CsmapBoPanelCategoryPanelCategoryKeys;
 import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
@@ -12,7 +13,7 @@ import org.osgi.service.component.annotations.Reference;
 		immediate = true,
 		property = {
 				"panel.app.order:Integer=260",
-				"panel.category.key=" + CsmapBoPanelCategoryPanelCategoryKeys.CONTROL_PANEL_CATEGORY
+				"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONTENT
 		},
 		service = PanelApp.class
 )
