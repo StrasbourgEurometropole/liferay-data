99499c68485abb3494ab59514a1943390764eae8
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2021-12-22 18:56:42 +0100

    #CSMAP BO : Changement du poids pour que les deux bo CSMap soient tjrs ensemble

--- modules/csmap/csmap-bo/csmap-bo-agenda/src/main/java/eu/strasbourg/portlet/agenda/csmap/panel/CsmapBOAgendaPanelApp.java
+++ modules/csmap/csmap-bo/csmap-bo-agenda/src/main/java/eu/strasbourg/portlet/agenda/csmap/panel/CsmapBOAgendaPanelApp.java
@@ -3,13 +3,16 @@ package eu.strasbourg.portlet.agenda.csmap.panel;
 import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.model.Portlet;
+import eu.strasbourg.portlet.agenda.csmap.constants.CsmapBoAgendaPortletKeys;
 import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 @Component(
 		immediate = true,
 		property = {
-				"panel.app.order:Integer=260",
+				"panel.app.order:Integer=259",
 				"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONTENT
 		},
 		service = PanelApp.class
@@ -21,4 +24,12 @@ public class CsmapBOAgendaPanelApp extends BasePanelApp {
 		return StrasbourgPortletKeys.CSMAP_BO_AGENDA;
 	}
 
+	@Override
+	@Reference(
+			target = "(javax.portlet.name=" + CsmapBoAgendaPortletKeys.CSMAPBOAGENDA + ")",
+			unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
 }
--- modules/csmap/csmap-bo/csmap-bo-place/src/main/java/eu/strasbourg/portlet/place/csmap/panel/CsmapBOPlacePanelApp.java
+++ modules/csmap/csmap-bo/csmap-bo-place/src/main/java/eu/strasbourg/portlet/place/csmap/panel/CsmapBOPlacePanelApp.java
@@ -3,13 +3,16 @@ package eu.strasbourg.portlet.place.csmap.panel;
 import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.model.Portlet;
+import eu.strasbourg.portlet.place.csmap.constants.CsmapBoPlacePortletKeys;
 import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 @Component(
 		immediate = true,
 		property = {
-				"panel.app.order:Integer=260",
+				"panel.app.order:Integer=259",
 				"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONTENT
 		},
 		service = PanelApp.class
@@ -21,4 +24,12 @@ public class CsmapBOPlacePanelApp extends BasePanelApp {
 		return StrasbourgPortletKeys.CSMAP_BO_PLACE;
 	}
 
+	@Override
+	@Reference(
+			target = "(javax.portlet.name=" + CsmapBoPlacePortletKeys.CSMAPBOPLACE + ")",
+			unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
 }
