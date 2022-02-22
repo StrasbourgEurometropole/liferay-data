7d01055c45722755e65b7e05d490f156b14e7dd8
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2022-02-22 18:15:15 +0100

    #Mantis 6037-6036-6035-6034 : SUppression ANalytics sur Strasbourg/Ete/Noel

--- themes/strasbourg-theme/custom/override/tarteaucitron.custom.js
+++ themes/strasbourg-theme/custom/override/tarteaucitron.custom.js
@@ -44,13 +44,6 @@ switch (window.tarteaucitronForceLanguage) {
         tarteaucitron.parameters.privacyUrl="https://int.strasbourg.eu/en/cookie-policy";
 }
 
-
-// Service Google Analytics (universal)
-// tarteaucitron.user.analyticsUa = 'UA-33301756-13'; -> français
-// tarteaucitron.user.analyticsUa = 'UA-36152381-1'; -> int
-tarteaucitron.user.analyticsMore = function () {  };
-(tarteaucitron.job = tarteaucitron.job || []).push('analytics');
-
 // Service Youtube
 //<div class="youtube_player" videoID="video_id" width="width" height="height" theme="theme (dark | light)" rel="rel (1 | 0)" controls="controls (1 | 0)" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" mute="mute (0 | 1)"></div>
 (tarteaucitron.job = tarteaucitron.job || []).push('youtube');
