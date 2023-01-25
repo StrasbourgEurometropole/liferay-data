107f3754aa376bf9d6ba08ae984ab76ace7475e7
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2023-01-25 19:20:32 +0100

    Gitlab-38559 : Log du possible timeout de l'appel RESID

--- modules/resid-web/src/main/java/eu/strasbourg/portlet/resid/dossier/DossiersWebService.java
+++ modules/resid-web/src/main/java/eu/strasbourg/portlet/resid/dossier/DossiersWebService.java
@@ -1,11 +1,16 @@
 package eu.strasbourg.portlet.resid.dossier;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Validator;
 import eu.strasbourg.utils.PasserelleHelper;
 import eu.strasbourg.utils.StrasbourgPropsUtil;
 
 import java.net.HttpURLConnection;
 
 public class DossiersWebService {
+
+	private static final Log _log = LogFactoryUtil.getLog(DossiersWebService.class);
 
 	public static DossiersResponse getResponse(String identifiantOpenId) {
 		return getResponse(identifiantOpenId, StrasbourgPropsUtil.getWebServiceDefaultTimeout());
@@ -17,11 +22,15 @@ public class DossiersWebService {
 			// On récupère le JSON
 			String url = StrasbourgPropsUtil.getResidantWebServiceURL() + identifiantOpenId;
 			HttpURLConnection httpConn = PasserelleHelper.readFromURL(url, timeOut);
-			if(httpConn.getContentType().contains("application/json")) {
+
+			if(Validator.isNull(httpConn.getContentType())) {
+				_log.error("RESID : Appel au webservice ne renvoie pas de Content-Type, possible Time Out");
+			}
+			else if (httpConn.getContentType().contains("application/json")) {
 				dossiersResponse = new DossiersResponse(PasserelleHelper.readJson(httpConn));
 			}
 		} catch (Exception ex) {
-			ex.printStackTrace();
+			_log.error(ex);
 		}
 
 		return dossiersResponse;
