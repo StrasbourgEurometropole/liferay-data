7e356d5411b47b9226394a16597cc39e698acb9e
messagR <messagr@hotmail.com>
2023-02-14 15:15:22 +0100

    all : gitlab 38756 -> Supprimer les entrées Google Analytics de tarteaucitron

--- themes/tipi-theme/src/js/tarteaucitron.custom.js
+++ themes/tipi-theme/src/js/tarteaucitron.custom.js
@@ -1,7 +1,2 @@
-// Service Google Analytics (gtag.js)
-tarteaucitron.user.gtagUa = 'UA-33301756-8';
-tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
-(tarteaucitron.job = tarteaucitron.job || []).push('gtag');
-
 // Service reCAPTCHA
 (tarteaucitron.job = tarteaucitron.job || []).push('recaptcha_ems');
